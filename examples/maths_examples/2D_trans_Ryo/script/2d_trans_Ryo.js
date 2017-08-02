a61f02eaec08ce0ac1cac83b4945256481d060d4
Ryo Kurashina <rk2014@ic.ac.uk>
2017-08-02 14:56:47 +0100

    Finally added some buttons to the html

--- examples/maths_examples/2D_trans_Ryo/script/2d_trans_Ryo.js
+++ examples/maths_examples/2D_trans_Ryo/script/2d_trans_Ryo.js
@@ -19,7 +19,6 @@ function rotation(vec,th) {
         x.push(newvec._data[0])
         y.push(newvec._data[1])
   }
-  console.log(x)
   return [x,y];
 }
 
@@ -29,21 +28,39 @@ function scale() {
   // Define variables and parameters
   var N = 50;
   var vec = arguments[0];
-  var x = math.zeros(N);
-  var y = math.zeros(N);
   if (arguments.length === 2){
-    x = numeric.linspace(vec[0],arguments[1]*vec[0],N);
-    y = numeric.linspace(vec[1],arguments[1]*vec[1],N);
+    var x = numeric.linspace(vec[0],arguments[1]*vec[0],N);
+    var y = numeric.linspace(vec[1],arguments[1]*vec[1],N);
   } else if (arguments.length === 3) {
-    x = numeric.linspace(vec[0],arguments[1]*vec[0],N);
-    y = numeric.linspace(vec[1],arguments[2]*vec[1],N);
+    var x = numeric.linspace(vec[0],arguments[1]*vec[0],N);
+    var y = numeric.linspace(vec[1],arguments[2]*vec[1],N);
   }
   return [x,y]
 }
 
+function skew(vec,axis) {
+  // Number of frames
+  var N = 50;
+  // If axis = 0 then skew in x-direction
+  if (axis === 0) {
+    var A = [[1,1],[0,1]];
+    var newvec = math.multiply(A,math.matrix(vec));
+    var x = numeric.linspace(vec[0],newvec._data[0],N);
+    var y = numeric.linspace(vec[1],newvec._data[1],N);
+    return [x,y]
+  }
+  else if (axis === 1) {
+    var A = [[1,0],[1,1]];
+    var newvec = math.multiply(A,math.matrix(vec));
+    var x = numeric.linspace(vec[0],newvec._data[0],N);
+    var y = numeric.linspace(vec[1],newvec._data[1],N);
+    return [x,y]
+  }
+}
+
 // Custom matrix transformation, arguments form matrix [[a,b],[c,d]]
 function custom(vec,a,b,c,d) {
-  N = 50;
+  var N = 50;
   var A = [[a,b],[c,d]];
   var x0 = vec[0];
   var y0 = vec[1];
@@ -73,33 +90,222 @@ function mylinspace(a,b,n) {
   return myinterval;
 }
 
-// Run arguments here for animation, try them all!
-var [x,y] = rotation([1,1],Math.PI)
-//var [x,y] = scale([0.5,0.3],2,3)
-//var [x,y] = custom([1,1],2,1,3,2)
+//// Run arguments here for animation, try them all!
+//var [x,y] = rotation([3,4],Math.PI)
+//var [x,y] = scale([2,1],2,3)
+//var [x,y] = custom([1,1],2,-1,3,2)
+//var [x,y] = skew([1,1],0);
 
-// Convery array to frames, print on console to check values...
-var frames = jsonformat(x,y)
-console.log(frames)
+//// Adjust x and y axes accordingly, remember to spread
+//var xmax = Math.max(Math.max(...x),Math.abs(Math.min(...x)));
+//var ymax = Math.max(Math.max(...y),Math.abs(Math.min(...y)));
+//// Include a bit of margin
+//var supermax = Math.max(xmax,ymax)+1;
+//
+//
+//// Convert array to frames, print on console to check values...
+//var frames = jsonformat(x,y)
 
-// Initial plot
-Plotly.plot('graph', [{
-  x: frames[0].data[0].x,
-  y: frames[0].data[0].y,
-  line: {simplify: false}}],
-  {xaxis: {range: [-2, 2]},
-    yaxis: {range: [-2, 2]}}
-);
+//// Initial plot
+//Plotly.plot('graph', [{
+//  x: frames[0].data[0].x,
+//  y: frames[0].data[0].y,
+//  line: {simplify: false}}],
+//  {xaxis: {range: [-supermax, supermax]},
+//    yaxis: {range: [-supermax, supermax]}}
+//);
+//
+//// Animation
+//Plotly.animate('graph', frames, {
+//  transition: {
+//    duration: 50,
+//    easing: 'linear'
+//  },
+//  frame: {
+//    duration: 50,
+//    redraw: false,
+//  },
+//  mode: 'immediate'
+//});
 
-// Animation
-Plotly.animate('graph', frames, {
-  transition: {
-    duration: 100,
-    easing: 'linear'
-  },
-  frame: {
-    duration: 100,
-    redraw: false,
-  },
-  mode: 'immediate'
-});
+///// NOW START TO DEFORM SQUARES WITH FUNCTIONS WE'VE ALREADY WRITTEN /////
+
+// Structure of this function goes:
+// (string for transformation, parameters)
+function squaretrans() {
+  if (arguments[0] === "rotate") {
+    var th = arguments[1];
+    var [x0,y0] = rotation([1,0],th);
+    var [x1,y1] = rotation([1,1],th);
+    var [x2,y2] = rotation([0,1],th);
+    return [x0,x1,x2,y0,y1,y2]
+  }
+  else if (arguments[0] === "scale") {
+    // If only 1 scale argument given
+    if (arguments.length === 2) {
+      var s = arguments[1];
+      var [x0,y0] = scale([1,0],s);
+      var [x1,y1] = scale([1,1],s);
+      var [x2,y2] = scale([0,1],s);
+      return [x0,x1,x2,y0,y1,y2]
+    }
+    else if (arguments.length === 3){
+      var s1 = arguments[1];
+      var s2 = arguments[2];
+      var [x0,y0] = scale([1,0],s1,s2);
+      var [x1,y1] = scale([1,1],s1,s2);
+      var [x2,y2] = scale([0,1],s1,s2);
+      return [x0,x1,x2,y0,y1,y2]
+    }
+  }
+  else if (arguments[0] === "custom") {
+    var a = arguments[1], b = arguments[2],
+     c = arguments[3], d = arguments[4];
+    var [x0,y0] = custom([1,0],a,b,c,d);
+    var [x1,y1] = custom([1,1],a,b,c,d);
+    var [x2,y2] = custom([0,1],a,b,c,d);
+    return [x0,x1,x2,y0,y1,y2]
+  }
+  else if (arguments[0] === "skew") {
+    var axis = arguments[1];
+    var [x0,y0] = skew([1,0],axis);
+    var [x1,y1] = skew([1,1],axis);
+    var [x2,y2] = skew([0,1],axis);
+    return [x0,x1,x2,y0,y1,y2]
+  }
+
+}
+
+// Test all sub functions
+//var myarray = squaretrans("rotate",2);
+//var myarray = squaretrans("scale",2);
+//var myarray = squaretrans("scale",2,3);
+//var myarray = squaretrans("custom",1,0,1,1);
+
+
+// Convert x,y arrays returned from functions in json format for animate
+function jsonformat2(x0,x1,x2,y0,y1,y2) {
+  var myjson = [];
+  var N = x0.length;
+  for (var i=0; i<N; i++) {
+    myjson.push({"data": [{"x": [0,x0[i],x1[i],x2[i],0],
+      "y": [0,y0[i],y1[i],y2[i],0]}],"name": 'frame'+parseInt(i)})
+  }
+  return myjson;
+}
+
+function plotterskew() {
+  var myarray = squaretrans("skew",1);
+  var frames = jsonformat2(...myarray);
+  // Initial plot
+  Plotly.newPlot('graph', [{
+    x: frames[0].data[0].x,
+    y: frames[0].data[0].y,
+    line: {simplify: false, color: 'rgb(0,62,116)'},
+    fill:'tonexty',
+    mode: 'lines'
+    }],
+    {xaxis: {range: [-3, 3]},
+      yaxis: {range: [-3, 3]}}
+  );
+
+  // Animation
+  Plotly.animate('graph', frames, {
+    transition: {
+      duration: 25,
+      easing: 'linear'
+    },
+    frame: {
+      duration: 25,
+      redraw: false,
+    },
+    mode: 'immediate'
+  });
+}
+
+function plotterscale() {
+  var myarray = squaretrans("scale",2,3);
+  var frames = jsonformat2(...myarray);
+  // Initial plot
+  Plotly.newPlot('graph', [{
+    x: frames[0].data[0].x,
+    y: frames[0].data[0].y,
+    line: {simplify: false, color: 'rgb(0,62,116)'},
+    fill:'tonexty',
+    mode: 'lines'
+    }],
+    {xaxis: {range: [-3, 3]},
+      yaxis: {range: [-3, 3]}}
+  );
+
+  // Animation
+  Plotly.animate('graph', frames, {
+    transition: {
+      duration: 25,
+      easing: 'linear'
+    },
+    frame: {
+      duration: 25,
+      redraw: false,
+    },
+    mode: 'immediate'
+  });
+}
+
+function plotterrotate() {
+  var myarray = squaretrans("rotate",1);
+  var frames = jsonformat2(...myarray);
+  // Initial plot
+  Plotly.newPlot('graph', [{
+    x: frames[0].data[0].x,
+    y: frames[0].data[0].y,
+    line: {simplify: false, color: 'rgb(0,62,116)'},
+    fill:'tonexty',
+    mode: 'lines'
+    }],
+    {xaxis: {range: [-3, 3]},
+      yaxis: {range: [-3, 3]}}
+  );
+
+  // Animation
+  Plotly.animate('graph', frames, {
+    transition: {
+      duration: 25,
+      easing: 'linear'
+    },
+    frame: {
+      duration: 25,
+      redraw: false,
+    },
+    mode: 'immediate'
+  });
+}
+
+function plottercustom() {
+  var myarray = squaretrans("custom",1,1,1,0);
+  var frames = jsonformat2(...myarray);
+  // Initial plot
+  Plotly.newPlot('graph', [{
+    x: frames[0].data[0].x,
+    y: frames[0].data[0].y,
+    line: {simplify: false, color: 'rgb(0,62,116)'},
+    fill:'tonexty',
+    mode: 'lines'
+    }],
+    {xaxis: {range: [-3, 3]},
+      yaxis: {range: [-3, 3]}}
+  );
+
+  // Animation
+  Plotly.animate('graph', frames, {
+    transition: {
+      duration: 25,
+      easing: 'linear'
+    },
+    frame: {
+      duration: 25,
+      redraw: false,
+    },
+    mode: 'immediate'
+  });
+}
