e7e64aa211169c171cd61c77bb4601be9c56ed5b
Dong-Woo Ko <dwko@outlook.com>
2017-08-05 13:01:51 +0100

    Updated on Visualisation

--- 3D Rotations/script/rotation3d.js
+++ 3D Rotations/script/rotation3d.js
@@ -1,8 +1,13 @@
 // Objects:
-function Line(point1, point2) {
-  this.x = [point1[0], point2[0]];
-  this.y = [point1[1], point2[1]];
-  this.z = [point1[2], point2[2]];
+function Line(points) {
+  this.x = [];
+  this.y = [];
+  this.z = [];
+  for (var i = 0; i  < points.length; ++i) {
+    this.x.push(points[i][0]);
+    this.y.push(points[i][1]);
+    this.z.push(points[i][2]);
+  }
 
   this.gObject = function(color) {
     var lineObject = {
@@ -48,16 +53,43 @@ function  rotationZ(angle) {
   return matrix;
 }
 
-var frameSize = 20;
+//Transformation algorithms
+function computeCompositeFrames(rotation1, rotation2, angle1, angle2, initialPoint, frameSize, color1, color2) {
+  intermediate1 = numeric.linspace(0.0, angle1, frameSize);
+  intermediate2 = numeric.linspace(0.0, angle2, frameSize);
+  var trace1 = [initialPoint];
+  var frames = [];
 
+  for (var i = 0, n = intermediate1.length; i < n; ++i) {
+    var newPoint = math.multiply(rotation1(intermediate1[i]), initialPoint);
+    trace1.push(newPoint);
+    var firstTrace = new Line(trace1);
+    frames.push(
+      [new Point(newPoint).gObject(),
+      firstTrace.gObject(color1),
+      new Line([[0., 0., 0.], [0., 0., 0.]]).gObject()]);
+  }
+var trace2 = [newPoint];
+  for (var i = 1, n = intermediate2.length; i < n; ++i) {
+    var newPoint2 = math.multiply(rotation2(intermediate2[i]), newPoint);
+    trace2.push(newPoint2);
+    frames.push(
+      [new Point(newPoint2).gObject(),
+      new Line(trace2).gObject(color2),
+      firstTrace.gObject(color1.slice(0, -1) + ",0.7)")]);
+  }
+  console.log()
+  return frames;
+}
+
+var frameSize = 20;
 var theta = Math.PI/2;
-//var t = numeric.linspace(0, theta, frameSize);
 var initialPoint = [2., 2., 2.];
 
 var data = [];
 
 for (var i = 0; i < 8; ++i) {
-  data.push(new Line([0.,0.,0.], [0.,0.,0.]).gObject("rgb(0,0,0)"));
+  data.push(new Line([[0.,0.,0.], [0.,0.,0.]]).gObject("rgb(0,0,0)"));
 }
 
 data.push(new Point(initialPoint).gObject("rgb(0,0,0)"));
@@ -65,8 +97,125 @@ data.push(new Point(initialPoint).gObject("rgb(0,0,0)"));
 for (var i = 0; i < 3; ++i) {
   uvec = [0., 0., 0.];
   uvec[i] = 1;
-  data.push(new Line([0.,0.,0], uvec).gObject("rgb(0,0,0)"));
+  data.push(new Line([[0.,0.,0], uvec]).gObject("rgb(0,0,0)"));
 }
 
-console.log(data);
-Plotly.plot('graph', data);
+var frameList1 = computeCompositeFrames(
+  rotationY,
+  rotationZ,
+  Math.PI/2,
+  Math.PI/2,
+  initialPoint,
+  frameSize,
+  "rgb(255,0,0)",
+  "rgb(0,0,255)");
+
+var frameList2 = computeCompositeFrames(
+  rotationZ,
+  rotationY,
+  Math.PI/2,
+  Math.PI/2,
+  initialPoint,
+  frameSize,
+  "rgb(0,0,255)",
+  "rgb(255,0,0)");
+
+var frames = [];
+for (var i = 0; i < frameList1.length; ++i) {
+  frames.push({
+    "data": [
+      frameList1[i][0],
+      frameList1[i][1],
+      frameList1[i][2],
+      frameList2[i][0],
+      frameList2[i][1],
+      frameList2[i][2]
+    ],
+    "name": i
+  })
+}
+
+var steps=[]
+for (var i = 0; i < frameSize; ++i) {
+  steps.push({
+    "label": "R1",
+    "method": "animate",
+    "args": [
+      [i],
+      {
+        "mode": "immediate",
+        "transition": {
+          "duration": 300,
+        },
+        "frame": {
+          "duration": 300,
+          "redraw": false
+        }
+      }
+    ]
+  })
+}
+
+for (var i = 0; i < frameSize - 1; ++i) {
+  steps.push({
+    "label": "R2",
+    "method": "animate",
+    "args": [
+      [i + frameSize],
+      {
+        "mode": "immediate",
+        "transition": {
+          "duration": 300,
+        },
+        "frame": {
+          "duration": 300,
+          "redraw": false
+        }
+      }
+    ]
+  })
+}
+
+var sliders= [{
+  "active": 0,
+  "pad": {"t":5},
+  "steps": steps
+}];
+
+var layout = {
+  "width": 750,
+  "height": 600,
+  "title": "Non-Communativeness of 3D Rotation",
+  "hovermode": "closest",
+  "updatemenus": [{
+    "x": 0.0,
+    "y": 0.15,
+    "xanchor": "right",
+    "yanchor": "top",
+    "showactive": false,
+    "type": "buttons",
+    "pad": {"t": 87, "r": 10},
+    "buttons": [
+      {
+        "method": "animate",
+        "args": [null, {"fromcurrent": true, "transition": {"duration": 50, "easing": "quadratic-in-out"}, "frame": {"duration": 50, "redraw":false}}],
+        "label": "Play"
+      },
+      {
+        "method": "animate",
+        "args":[[null], {"mode": "immediate", "transition": {"duration": 0}, "frame": {"duration": 0, "redraw": false}}],
+        "label": "Pause"
+      }
+  ]
+}],
+"showlegend": false,
+"sliders": sliders
+}
+
+var figure = {
+  "data": data,
+  "frames": frames,
+  "layout": layout
+}
+console.log(figure);
+Plotly.plot('graph', figure);
