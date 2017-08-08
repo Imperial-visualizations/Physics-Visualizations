754ee595cf3ec235a13aea287e00b49ee03c8f31
Dong-Woo Ko <dwko@outlook.com>
2017-08-08 14:13:05 +0100

    drafted version of "on a sphere"

--- 3D Rotations/script/rotation3d.js
+++ 3D Rotations/script/rotation3d.js
@@ -21,7 +21,6 @@ function Line(points) {
     return lineObject;
   }
 }
-
 function Point(position) {
   this.position = position;
   this.gObject = function(color) {
@@ -36,7 +35,6 @@ function Point(position) {
     return pointObject;
   }
 }
-
 function Sphere(radius) {
   this.radius = radius;
   var meshSize = 20;
@@ -70,13 +68,11 @@ function rotationX(angle) {
   var matrix = [[1, 0, 0], [0, Math.cos(angle), -Math.sin(angle)], [0, Math.sin(angle), Math.cos(angle)]];
   return matrix;
 }
-
 function rotationY(angle) {
   var matrix = [[Math.cos(angle), 0, Math.sin(angle)], [0, 1, 0], [-Math.sin(angle), 0, Math.cos(angle)]];
   return matrix;
 }
-
-function  rotationZ(angle) {
+function rotationZ(angle) {
   var matrix = [[Math.cos(angle), -Math.sin(angle), 0], [Math.sin(angle), Math.cos(angle), 0], [0, 0 ,1]];
   return matrix;
 }
@@ -86,17 +82,14 @@ function scaleX(factor) {
   var matrix = [[factor, 0, 0], [0, 1, 0], [0, 0, 1]];
   return matrix;
 }
-
 function scaleY(factor) {
   var matrix = [[1, 0, 0], [0, factor, 0], [0, 0, 1]];
   return matrix;
 }
-
-function  scaleZ(factor) {
+function scaleZ(factor) {
   var matrix = [[1, 0, 0], [0, 1, 0], [0, 0 ,factor]];
   return matrix;
 }
-
 function scaleAll(factorAll) {
   var matrix = [[factorAll[0], 0, 0], [0, factorAll[1], 0], [0, 0, factorAll[2]]];
   return matrix;
@@ -123,7 +116,6 @@ function computeFrames(transformation, start, end, initialPoint, frameSize) {
   }
   return frames;
 }
-
 function computeCompositeFrames(rotation1, rotation2, angle1, angle2, initialPoint, frameSize, color1, color2) {
   var intermediate1 = numeric.linspace(0.0, angle1, frameSize);
   var intermediate2 = numeric.linspace(0.0, angle2, frameSize);
@@ -139,7 +131,7 @@ function computeCompositeFrames(rotation1, rotation2, angle1, angle2, initialPoi
       firstTrace.gObject(color1),
       new Line([[0., 0., 0.], [0., 0., 0.]]).gObject()]);
   }
-var trace2 = [newPoint];
+  var trace2 = [newPoint];
   for (var i = 1, n = intermediate2.length; i < n; ++i) {
     var newPoint2 = math.multiply(rotation2(intermediate2[i]), newPoint);
     trace2.push(newPoint2);
@@ -150,33 +142,6 @@ var trace2 = [newPoint];
   }
   return frames;
 }
-
-//Global Initial Parameters:
-var initialPoint = [2., 2., 2.];
-var radius = 2*Math.sqrt(3);
-var animatePause = false;
-
-function init() {
-  var data = [];
-
-  for (var i = 0; i < 8; ++i) {
-    data.push(new Line([[0.,0.,0.], [0.,0.,0.]]).gObject("rgb(0,0,0)"));
-  }
-
-  data.push(new Point(initialPoint).gObject("rgb(0,0,0)"));
-
-  for (var i = 0; i < 3; ++i) {
-    uvec = [0., 0., 0.];
-    uvec[i] = 1;
-    data.push(new Line([[0.,0.,0], uvec]).gObject("rgb(0,0,0)"));
-  }
-
-  sphere = new Sphere(radius);
-  data.push(sphere.gObject("rgb(0,62,116)", "rgb(255,255,255)"));
-
-  return data;
-}
-
 function computeCommute(rotation1, rotation2, theta1, theta2, frameSize) {
   var frameList1 = computeCompositeFrames(
     rotation1, rotation2,
@@ -281,34 +246,15 @@ function computeCommute(rotation1, rotation2, theta1, theta2, frameSize) {
   return [frames, layout];
 }
 
-function plotGraph(data, [frames, layout]) {
-  var figure = {
-    "data": data,
-    "frames": frames,
-    "layout": layout
-  }
+//Global Initial Parameters:
+var initialPoint = [2., 2., 2.];
+var radius = 2*Math.sqrt(3);
+var animatePause = false;
 
-  Plotly.newPlot('graph', figure);
-}
-
-function plotInit() {
-  var data = init();
-
-  var layout = {
-    "width": 700, "height": 600,
-    "margin": {l:0, r:0, t:0, b:50},
-    "hovermode": "closest",
-    "showlegend": false
-  }
-
-  var figure = {"data": data, "layout": layout}
-  Plotly.newPlot('graph', figure,);
-}
-
-// Rotation:
+//Hide/Show Option (Rotation) - for better interface
+// Rotation
 var rotationType = 0; //To keep track of which button is active also keep track of toggle
 function hideRotate() {
-  $('.rotateSlider').hide();
   $('.rotateCP1').hide();
   $('.rotateCP2').hide();
   $('.rotateCP3').hide();
@@ -317,118 +263,44 @@ function hideRotate() {
 function showRotate(axis) {
   if (axis === 1 & axis !== rotationType){
     hideRotate();
-    $('.rotateSlider').slideToggle(600);
     $('.rotateCP1').slideToggle(600);
-    $('.rotateCommute').slideToggle(600);
   } else if (axis === 2 & axis !== rotationType){
     hideRotate();
-    $('.rotateSlider').slideToggle(600);
     $('.rotateCP2').slideToggle(600);
-    $('.rotateCommute').slideToggle(600);
   } else if (axis === 3 & axis !== rotationType){
     hideRotate();
-    $('.rotateSlider').slideToggle(600);
     $('.rotateCP3').slideToggle(600);
-    $('.rotateCommute').slideToggle(600);
   }
   rotationType = axis;
 }
-//Reflection:
-function hideReflect() {
-  $('.reflect3D').hide();
-}
-function showReflect() {
-  $('.reflect3D').slideToggle(600);
-}
-//Scale:
-function hideScale() {
-  $('.scale3D').hide();
-  $('.scaleSlider').hide();
-}
-function showScale() {
-  $('.scale3D').slideToggle(600);
-  $('.scaleSlider').slideToggle(600);
+var rotateMain = true;
+function revealRotate(axis) {
+  hideCommute();
+  plotInit();
+  showRotate(axis);
 }
 //Commute
 function hideCommute() {
-  $('.commuteSliders').hide();
+  $('.rotateCommute').hide();
 }
 function showCommute() {
-  $('.commuteSliders').slideToggle(600);
-}
-
-function main() {
-  plotInit();
-  $('.rotate3D').hide();
-  hideRotate();
-  hideReflect();
-  hideScale();
-}
-
-//Rotation Callbacks
-var rotateMain = false;
-function revealRotate(axis) {
-  hideReflect();
-  hideScale();
-  hideCommute();
-  plotInit();
-  if (!rotateMain) {
-    $('.rotate3D').slideToggle(600);
-    rotateMain = true;
-  }
-  showRotate(axis);
+  $('.rotateCommute').slideToggle(600);
 }
 function revealCommute() {
-  if (rotationType === 4) {
-    revealRotate(1);
-    return;
-  }
+  $('.rotate3D').slideToggle(600);
   $('.rotateSlider').slideToggle(600);
-  if (rotationType === 1) {
-    $('.rotateCP1').slideToggle(600);
-  } else if (rotationType === 2) {
-    $('.rotateCP2').slideToggle(600);
-  } else if (rotationType === 3) {
-    $('.rotateCP3').slideToggle(600);
-  }
-  rotationType = 4;
+  hideRotate();
   showCommute();
-
   plotCommute();
-}
-function plotCommute() {
-  var data1, data2;
-  var theta1 = document.getElementById('rotator1').value * Math.PI;
-  var theta2 = document.getElementById('rotator1').value * Math.PI;
-
-  var frameSize = 30;
-  data1 = init();
-  data2 = computeCommute(rotationY, rotationZ, theta1, theta2, frameSize);
-  plotGraph(data1, data2);
-}
-
-//Refection Callbacks
-function revealReflect() {
-  rotateMain = false;
   rotationType = 0;
-  $('.rotate3D').hide();
-  hideRotate();
-  hideScale();
-  plotInit();
-  showReflect();
+}
+function unrevealCommute() {
+  $('.rotate3D').slideToggle(600);
+  $('.rotateSlider').slideToggle(600);
+  revealRotate(1);
 }
 
-//Scale Callbacks
-function revealScale() {
-  rotateMain = false;
-  rotationType = 0;
-  $('.rotate3D').hide();
-  hideRotate();
-  hideReflect();
-  plotInit();
-  showScale();
-}
-
+//Animation Options
 function startAnimation (frames) {
   Plotly.animate('graph', frames,{
     fromcurrent: true,
@@ -443,12 +315,55 @@ function startAnimation (frames) {
     mode: "immediate"
   });
 }
-
 function stopAnimation () {
   Plotly.animate('graph', [], {mode: 'next'});
 }
 
-// Control points: Slider, play button etc...
+// Plots
+function init() {
+  var data = [];
+
+  for (var i = 0; i < 8; ++i) {
+    data.push(new Line([[0.,0.,0.], [0.,0.,0.]]).gObject("rgb(0,0,0)"));
+  }
+
+  data.push(new Point(initialPoint).gObject("rgb(0,0,0)"));
+
+  for (var i = 0; i < 3; ++i) {
+    uvec = [0., 0., 0.];
+    uvec[i] = 1;
+    data.push(new Line([[0.,0.,0], uvec]).gObject("rgb(0,0,0)"));
+  }
+
+  sphere = new Sphere(radius);
+  data.push(sphere.gObject("rgb(0,62,116)", "rgb(255,255,255)"));
+
+  return data;
+} //Initialise
+function plotInit() {
+  var data = init();
+
+  var layout = {
+    "width": 700, "height": 600,
+    "margin": {l:0, r:0, t:0, b:50},
+    "hovermode": "closest",
+    "showlegend": false,
+    "scene":{
+      xaxis: {
+        range: [-4, 4]
+      },
+      yaxis: {
+        range: [-4, 4]
+      },
+      zaxis: {
+        range: [-4, 4]
+      }
+    }
+  }
+
+  var figure = {"data": data, "layout": layout}
+  Plotly.newPlot('graph', figure,);
+}
 function plotInitialSphere(frames) {
   sphere = new Sphere(radius);
 
@@ -477,7 +392,18 @@ function plotInitialSphere(frames) {
       "width": 700, "height": 600,
       "margin": {l:0, r:0, t:0, b:50},
       "hovermode": "closest",
-      "showlegend": false
+      "showlegend": false,
+      "scene":{
+        xaxis: {
+          range: [-4, 4]
+        },
+        yaxis: {
+          range: [-4, 4]
+        },
+        zaxis: {
+          range: [-4, 4]
+        }
+      }
     })
 }
 function plotRotate(axis) {
@@ -493,8 +419,8 @@ function plotRotate(axis) {
   }
   plotInitialSphere(frames);
   startAnimation(frames);
+  displayRotationMatrix();
 }
-
 function plotReflect(plane) {
   var frames;
   var frameSize = 10;
@@ -510,7 +436,6 @@ function plotReflect(plane) {
   //Plotly.plot('graph', [new Line([[0.,0.,0], [2,2,2]]).gObject("rgb(0,0,0)")]);
   startAnimation(frames);
 }
-
 function plotScale(direction) {
   var factor  = document.getElementById('scaler').value;
   var frames;
@@ -527,5 +452,80 @@ function plotScale(direction) {
   plotInitialSphere(frames);
   startAnimation(frames);
 }
+//Plot for commute
+function plotGraph(data, [frames, layout]) {
+  var figure = {
+    "data": data,
+    "frames": frames,
+    "layout": layout
+  }
 
+  Plotly.newPlot('graph', figure);
+} //only to plot commute!
+function plotCommute() {
+  var data1, data2;
+  var theta1 = document.getElementById('rotator1').value * Math.PI;
+  var theta2 = document.getElementById('rotator2').value * Math.PI;
+  var frameSize = 20;
+  data1 = init();
+  data2 = computeCommute(rotationY, rotationZ, theta1, theta2, frameSize);
+  plotGraph(data1, data2);
+}
+
+//Slider Value and Matrix grid Value
+function makeTableHTML(myArray) {
+    var result = "<table><tbody>";
+    for(var i=0; i<myArray.length; i++) {
+        result += "<tr>";
+        for(var j=0; j<myArray[i].length; j++){
+            result += "<td>"+myArray[i][j]+"</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</tbody></table>";
+    return result;
+}
+function displayRotationMatrix() {
+  var angle = document.getElementById("rotator").value;
+  var cosAngle = "cos("+String(angle)+"π"+")", sinAngle = "sin(" + String(angle)+"π"+")";
+  if (rotationType === 1) {
+    document.getElementById("rotateMatrix").innerHTML=makeTableHTML(
+      [
+        ["1", "0", "0"],
+        ["0", cosAngle, "-"+sinAngle],
+        ["0", sinAngle, cosAngle]
+      ]
+    )
+  } else if (rotationType === 2) {
+    document.getElementById("rotateMatrix").innerHTML=makeTableHTML(
+      [
+        [cosAngle, "0", sinAngle],
+        ["0", "1", "0"],
+        ["-"+sinAngle, "0", cosAngle]
+      ]
+    )
+  } else if (rotationType === 3) {
+    document.getElementById("rotateMatrix").innerHTML=makeTableHTML(
+      [
+        [cosAngle, "-"+sinAngle, "0"],
+        [sinAngle, cosAngle, "0"],
+        ["0", "0" ,"1"]
+      ]
+    )
+  }
+}
+function displayReflectionMatrix() {
+}
+function displayScaleMatrix() {
+}
+
+function main() {
+  $("input[type=range]").each(function () {
+    $(this).on('input', function(){
+          $("#"+$(this).attr("id") + "Display").text(  $(this).val() + $("#"+$(this).attr("id")+"Display").attr("data-unit")  );
+    });
+  });
+  plotInit();
+  revealRotate(1); //Since Rotate is the active tab (html)
+}
 $(document).ready(main);
