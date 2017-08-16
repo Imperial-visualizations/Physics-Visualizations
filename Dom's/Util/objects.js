127d455854d585babf8433ed3b82cdbb96018d07
Dong-Woo Ko <dwko@outlook.com>
2017-08-16 16:16:56 +0100

    Completed the first draft of COB vis

---
+++ Dom's/Util/objects.js
@@ -0,0 +1,124 @@
+//Objects:
+function Line(points) {
+    this.x = [];
+    this.y = [];
+    this.z = [];
+    for (var i = 0; i  < points.length; ++i) {
+        this.x.push(points[i][0]);
+        this.y.push(points[i][1]);
+        this.z.push(points[i][2]);
+    }
+
+    this.gObject = function(color) {
+        var lineObject = {
+            "type": "scatter3d",
+            "mode": "lines",
+            "x": this.x,
+            "y": this.y,
+            "z": this.z,
+            "line": {"color": color, "width": 7}
+        }
+        return lineObject;
+    }
+}
+function Point(position) {
+    this.position = position;
+    this.gObject = function(color, symbol="circle") {
+        pointObject = {
+            "type": "scatter3d",
+             "mode": "markers",
+             "x": [this.position[0]],
+             "y": [this.position[1]],
+             "z": [this.position[2]],
+             "marker": {"color": color, "size": 7, "symbol": symbol}
+        }
+        return pointObject;
+    }
+}
+function Sphere(radius) {
+    this.radius = radius;
+    var meshSize = 20;
+    var theta = numeric.linspace(0, Math.PI, meshSize);
+    var phi = numeric.linspace(0, 2*Math.PI, meshSize);
+    this.x = [], this.y = [], this.z = [];
+    for(var i = 0; i < meshSize; ++i) {
+        this.x[i] = [], this.y[i] = [], this.z[i] = [];
+        for(var j = 0; j < meshSize; ++j) {
+            this.x[i].push(radius*Math.cos(phi[i])*Math.sin(theta[j]));
+            this.y[i].push(radius*Math.sin(phi[i])*Math.sin(theta[j]));
+            this.z[i].push(radius*Math.cos(theta[j]));
+        }
+    }
+    this.gObject = function(color1, color2) {
+        var sphere = {
+            "type": "surface",
+            "x": this.x,
+            "y": this.y,
+            "z": this.z,
+            "showscale": false,
+            "opacity": 0.7,
+            "colorscale": [[0.0, color1], [1.0, color2]]
+        }
+        return sphere;
+    }
+}
+function Cylinder(radius, height){
+    this.radius = radius;
+    this.height = height;
+    var meshSize = 20;
+    var phi = numeric.linspace(0, 2*Math.PI, meshSize);
+    this.x = [], this.y = [], this.z = [];
+    var hval = numeric.linspace(-height, height, meshSize);
+    for(var i = 0; i < meshSize; ++i) {
+        this.x[i] = [], this.y[i] = [], this.z[i] = [];
+        for(var j = 0; j < meshSize; ++j) {
+            this.x[i].push(radius*Math.cos(phi[j]));
+            this.y[i].push(radius*Math.sin(phi[j]));
+            this.z[i].push(hval[i]);
+        }
+    }
+    this.xt = [], this.yt = [];
+    for(var i = 0; i < meshSize; ++i) {
+        this.xt.push(radius*Math.cos(phi[i]));
+        this.yt.push(radius*Math.sin(phi[i]));
+    }
+
+    this.gObjectCurve = function(color1, color2) {
+        var curve = {
+            "type": "surface",
+            "x": this.x,
+            "y": this.y,
+            "z": this.z,
+            "showscale": false,
+            "opacity": 0.7,
+            "colorscale": [[0.0, color1], [1.0, color2]]
+        }
+        return curve;
+    }
+    this.gObjectTop = function(color) {
+        var top = {
+            type: "scatter3d",
+            mode: "lines",
+            x: this.xt,
+            y: this.yt,
+            z: this.z[meshSize - 1],
+            line: {color: color.slice(0, -1) + ",0.2)", width: 1},
+            surfaceaxis: 2,
+            opacity: 0.5
+        }
+        return top;
+    }
+    this.gObjectBottom = function(color) {
+        var bottom = {
+            type: "scatter3d",
+            mode: "lines",
+            x: this.xt,
+            y: this.yt,
+            z: this.z[0],
+            line: {color: color.slice(0, -1) + ",0.7)", width: 1},
+            surfaceaxis: 2,
+            opacity: 0.7
+        }
+        return bottom;
+    }
+}
