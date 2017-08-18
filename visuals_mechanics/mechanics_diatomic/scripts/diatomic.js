8d0d10864a31b84f115c715c0a46382ce22cce33
Akash B <akashb50@yahoo.co.uk>
2017-08-18 13:15:18 +0100

    diatomic - diatomic.js and potential.js
    Code cleaned up a little.
    Revamped method of dealing with heterogeneous diatomic molecules.

--- visuals_mechanics/mechanics_diatomic/scripts/diatomic.js
+++ visuals_mechanics/mechanics_diatomic/scripts/diatomic.js
@@ -4,7 +4,6 @@
  * @param pos: Vector with Cartesian position coordinates
  * @param radius: Radius of atom.
  * @param mass: Mass of atom.
- * @param potential: Object defining potential type at atom (e.g. LJ, harmonic, etc.)
  * @param color: Color of atom for phaser code.(cosmetic only)
  * @constructor: Atom
  */
@@ -22,130 +21,90 @@ Atom = function(pos, radius, mass, color) {
     }
 
     this.color = color;
-    this.radius = radius;                            // Atom radius.
-    this.mass = mass;                              // Atom mass.
+    this.radius = radius;                               // Atom radius.
+    this.mass = mass;                                   // Atom mass.
     this.sprite = addAtom(this);
 };
 
 /**
  * Class to make molecule out of atoms, with a spring constant that defines strength of bonds, and instantiated with
  * rotational and vibrational E.
- * @param atoms: Array containing all atoms in molecules.
+ * @param a1: Atom 1.
+ * @param a2: Atom 2.
  * @param potential: Potential describing bond between atoms.
- * @param vibrational_E: Vibrational kinetic energy.
- * @param rotational_E: Rotational kinetic energy.
+ * @param keVib_0: Vibrational kinetic energy at t=0.
+ * @param keRot_0: Rotational kinetic energy at t=0.
  * @constructor
  */
-Molecule = function(a1,a2, potential, keVib_0,keRot_0) {
+Molecule = function(a1, a2, potential, keVib_0, keRot_0) {
 
     // Checking objects of class Atom passed in in the list.
     if (a1.constructor !== Atom || a2.constructor !== Atom){
         console.error("Can't run simulation without two valid atom objects");
     }
 
-    this.V = potential;
-
-    // Finding total mass of the system.
-    this.tot_m = function () {
-        return a1.mass + a2.mass;
-    };
-    // Finding system's reduced mass.
-    this.reducedM = function () {
-        return a1.mass*a2.mass /(a1.mass+a2.mass);
-    };
+    this.V = potential;                                          // Setting potential between the diatoms.
+    this.tot_m = a1.mass + a2.mass;                              // Finding total mass of the system.
+    this.reducedM = a1.mass * a2.mass / (this.tot_m);            // Finding system's reduced mass.
 
     this.init_r_0 = function () {
         var val = this.V.getR_0();
 
-        for(var i =0; i < 10; i++){
-            val -= (this.reducedM()*Math.pow(this.omega,2)*Math.pow(val,14)
-                - 12*this.V.e*Math.pow(this.V.getR_0()*val,6)- 12*this.V.e*Math.pow(this.V.getR_0(),12))/
-                    (14*this.reducedM()*Math.pow(this.omega,2)*Math.pow(val,13)-72*this.V.e*Math.pow(val,5) *
-                        Math.pow(this.V.getR_0(),6));
+        for (var i =0; i < 10; i++) {
+            val -= (this.reducedM * Math.pow(this.omega, 2) * Math.pow(val, 14)
+                - 12 * this.V.e * Math.pow(this.V.getR_0() * val, 6) - 12 * this.V.e * Math.pow(this.V.getR_0(), 12))/
+                    (14 * this.reducedM * Math.pow(this.omega,2) * Math.pow(val,13)-72 * this.V.e * Math.pow(val, 5) *
+                        Math.pow(this.V.getR_0(), 6));
         }
         return val;
     };
 
-    /**
-     * @return {number}
-     */
-    this.I_0 = function () {
-        return this.reducedM() * Math.pow(this.V.getR_0(),2);
-    };
-    this.omega_0 = function () {
-        return Math.sqrt( 2*keRot_0/this.I);
-    };
-    this.I = this.I_0();
-    this.omega = this.omega_0();
-    this.L = this.I * this.omega;
+    this.I = this.reducedM * Math.pow(this.V.getR_0(),2);       // Calculate initial Moment of Inertia.
+    this.omega = Math.sqrt(2 * keRot_0 / this.I);               // Calculate initial angular velocity.
+    this.L = this.I * this.omega;                               // Calculate angular momentum (conserved).
+    this.r = new Vector([0,1]).multiply(this.init_r_0());       // Initial radius, due to centrifugal distortion
+    this.v = Math.sqrt(2 * keVib_0 / this.reducedM);            // Initial linear velocity of molecule.
 
-    this.r = new Vector([0,1]).multiply(this.init_r_0());
-    this.v = Math.sqrt(2* keVib_0 / this.reducedM());
-
-    a1.pos = this.r.multiply(a1.mass/this.tot_m());
-    a2.pos = this.r.multiply(-a2.mass/this.tot_m());
-
-
-    console.log(this.r.toString());
-    // // Magnitude of linear speed for each Atom object.
-    // this.p_i = Math.sqrt((this.E_v * 2 * this.tot_m()) / (Math.pow(this.atoms.length, 2)));
-    // for (var i = 0; i < this.atoms.length; i++) {
-    //     this.atoms[i].v = this.p_i / this.atoms[i].m;                   // Setting each atom's speed.
-    // }
-
+    // Finding initial coordinates of atoms in CoM frame.
+    a1.pos = this.r.multiply(a1.mass / this.tot_m);
+    a2.pos = this.r.multiply(-a2.mass / this.tot_m);
 };
 
 /** ================================================= Class Methods ==================================================*/
-/**
- * Calculates the scalar distance between a pair of atoms.
- * @param atom1: Atom
- * @param atom2: Another atom
- * @returns {Vector} separation, r.
- */
-Molecule.prototype.calcSeparation = function(atom1, atom2) {
-    var r = atom1.pos.subtract(atom2.pos);
-    return new Vector(r)
-};
-
-/**
- * Calculates the unit vector between two atom positions.
- * @param atom1: Atom
- * @param atom2: Another atom
- * @returns {Vector} Unit vector
- */
-Molecule.prototype.calcUnitVect = function(atom1, atom2) {
-    var r = atom1.pos.subtract(atom2.pos);
-    return new Vector(r.unit())
-};
-
 /**
  * Function to calculate extended coordinates due to vibrational energy.
  * @param dT: Time elapsed since previous timestep.
  */
 Molecule.prototype.calcExtCoords = function (dT) {
-    var a = this.V.calcF(this.r.mag())/this.reducedM() + Math.pow(this.omega,2)*this.r.mag();
-    this.v = a*dT;
-    this.r = this.r.add(this.r.unit().multiply(this.v * dT));
+    // Acceleration due to potential.
+    var lin_a = this.V.calcF(this.r.mag())/this.reducedM;
+
+    // Centrifugal force due to rotation of molecule.
+    var centrifugal_a = Math.pow(this.omega, 2) * this.r.mag();
+
+    var a = lin_a + centrifugal_a;                          // Total linear acceleration.
+    this.v += a * dT;                                       // Using "v = u + at"
+    var dr = this.r.unit().multiply(this.v * dT);           // Displacement vector.
+    this.r = this.r.add(dr);                                // Separation vector updated.
 };
 
 /**
- * Calculates the potential energy of the system using the potential function instantiated to the molecule.
- * @returns {Number} System PE.
+ * Updates the separation vector, r, and the coordinates of the atoms.
+ * @returns
  */
 Molecule.prototype.update = function(deltaTime){
+
+    // Recalculate MoI and angular velocity.
     this.I = this.calcMoI();
     this.omega = this.calcAngVel();
+
+    // Rotate and vibrate.
     this.calcRotCoords(deltaTime);
     this.calcExtCoords(deltaTime);
-    a1.pos = this.r.multiply(a1.mass/this.tot_m());
-    a2.pos = this.r.multiply(-a2.mass/this.tot_m());
-};
 
-
-
-Molecule.prototype.calcPE = function() {
-    this.PE = this.V.calcV(this.r.mag());                                // Potential energy calculation.
-    return this.PE
+    // Update atom coordinates in CoM frame.
+    a1.pos = this.r.multiply(a1.mass / this.tot_m);
+    a2.pos = this.r.multiply(-a2.mass / this.tot_m);
 };
 
 /**
@@ -156,7 +115,6 @@ Molecule.prototype.calcPE = function() {
 Molecule.prototype.calcRotCoords = function(dt) {
     this.r = this.r.rotate(this.omega * dt);
 };
-
 
 /**
  * Calculates the rotational kinetic energy of system, using the conservation of Angular Momentum.
@@ -173,7 +131,7 @@ Molecule.prototype.calcRotKE = function() {
  * @returns {number} I
  */
 Molecule.prototype.calcMoI = function() {
-    return this.reducedM() * Math.pow(this.r.mag(),2);                                                       // Return Moment of Inertia (scalar).
+    return this.reducedM * Math.pow(this.r.mag(),2);                              // Return Moment of Inertia (scalar).
 };
 
 /**
@@ -183,28 +141,3 @@ Molecule.prototype.calcMoI = function() {
 Molecule.prototype.calcAngVel = function () {
     return this.L / this.I;
 };
-
-/**
- * Calculates Centre of Mass coordinates.
- * @returns {Vector} Centre of Mass
- */
-Molecule.prototype.calcCOM = function() {
-    var total_m = 0;                                                    // Total mass of system.
-
-    var total_mx = [];                                                  // Total mass * position (mx) of system.
-    for (var j = 0; j < this.atoms[0].pos.items.length; j++) {
-        total_mx.push(0)
-    }
-    total_mx = new Vector(total_mx);                                    // mx initialised as 0 vector.
-
-    // Summing mx and m.
-    for (var i = 0; i < this.atoms.length; i++) {
-        total_m += this.atoms[i].m;
-        var mx = this.atoms[i].pos.multiply(this.atoms[i].m);
-        total_mx = total_mx.add(mx);
-    }
-
-    this.COM = total_mx.multiply(1 / total_m);                          // Updating instance COM coords.
-    return this.COM;                                                    // Return Vector coordinates of COM.
-};
-
