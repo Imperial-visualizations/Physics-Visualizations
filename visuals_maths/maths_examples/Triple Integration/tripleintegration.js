60ea482369b7fcc78a8931b019ac5acd6fa009ba
Unknown <rohan.kamath9@gmail.com>
2017-08-21 13:59:07 +0100

    /maths: Auto stash before merge of "visualizations/maths" and "origin/visualizations/maths"
    Some more changes  to the triple integration

--- visuals_maths/maths_examples/Triple Integration/tripleintegration.js
+++ visuals_maths/maths_examples/Triple Integration/tripleintegration.js
@@ -1,4 +1,4 @@
-function Zintegratorelement(xinit, yinit,zinit,number){
+function Zintegratorelement(xinit, yinit,zinit,number, a , b , c ){
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
             zz.push(zinit[j] +(0.1*number))     
@@ -12,14 +12,16 @@ function Zintegratorelement(xinit, yinit,zinit,number){
         j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
         k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
         opacity: 1.0,
-        color: 'rgb(0,0,0)',  
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
+        intensity: [0, 0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 1],
 
-        showscale: false, }
+        showscale: false}
     return newcube
 }
 
-
-function Zintegratortotal(xinit, yinit,zinit,number){
+function Zintegratortotal(xinit, yinit,zinit,number,a,b,c){
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
             if (zinit[j] != 0){
@@ -37,20 +39,23 @@ function Zintegratortotal(xinit, yinit,zinit,number){
         i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
         j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
         k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
-        opacity: 1.0,
-        color: 'rgb(165,25,0)',  
+        opacity: 0.8,
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
 
         showscale: false, }
     return newcube
 }
 
-function Yintegratorelement(xinit, yinit, zinit, number){
+function Yintegratorelement(xinit, yinit, zinit, number,a,b,c){
     yy = []
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
             ylim = yinit[j] +(0.1*number)
             if (zinit[j] != 0) {
-                zlim = 1 - ylim
+                zlim = c*(1 - (ylim/b))
             } else {
                 zlim = 0 
             }
@@ -62,18 +67,20 @@ function Yintegratorelement(xinit, yinit, zinit, number){
         x: xinit,
         y: yy,
         z: zz,
-        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
-        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
-        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        i: [7, 2, 5, 0, 3, 5, 2, 4, 0, 0, 4, 4],
+        j: [6, 6, 0, 5, 5, 3, 4, 2, 2, 3, 6, 5],
+        k: [2, 3, 1, 4, 1, 6, 7, 0, 3, 1, 7, 6],
         intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
-        opacity: 0.8,
-        color: 'rgb(0,0,0)',  
+        opacity: 1.0,
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
 
         showscale: false,}
     return newcube
 }
 
-function Yintegratortotal(xinit, yinit, zinit, number){
+function Yintegratortotal(xinit, yinit, zinit, number,a,b,c){
     yy = []
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
@@ -83,7 +90,7 @@ function Yintegratortotal(xinit, yinit, zinit, number){
                 ylim = 0 
             }
             if (zinit[j] != 0) {
-                zlim = 1 - ylim
+                zlim = c*(1 - (ylim/b))
             } else {
                 zlim = 0 
             }
@@ -95,30 +102,34 @@ function Yintegratortotal(xinit, yinit, zinit, number){
         x: xinit,
         y: yy,
         z: zz,
-        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
-        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
-        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        i: [7, 2, 5, 0, 3, 5, 2, 4, 0, 0, 4, 4],
+        j: [6, 6, 0, 5, 5, 3, 4, 2, 2, 3, 6, 5],
+        k: [2, 3, 1, 4, 1, 6, 7, 0, 3, 1, 7, 6],
         intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
         opacity: 0.8,
-        color: 'rgb(210,64,0)',  
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
 
+        opacity: 1.0,
         showscale: false,}
     return newcube
 }
 
-function Xintegratorelement(xinit, yinit, zinit, number){
+function Xintegratorelement(xinit, yinit, zinit, number,a,b,c){
     xx = []
     yy = []
     zz = []
     for (var j = 0; j <6 ; j++){
         xlim = xinit[j] + (0.1*number)
         if (yinit[j] != 0) {
-                ylim = 1 - xlim
+                ylim = b*(1 - (xlim/a))
             } else {
                 ylim = 0 
             }
         if (zinit[j] != 0) {
-                zlim = 1 - (ylim+xlim)
+                zlim = 1 - c*((ylim/b)+(xlim/a))
             } else {
                 zlim = 0 
             }
@@ -135,13 +146,18 @@ function Xintegratorelement(xinit, yinit, zinit, number){
                 j: [5,1,3,3,0,2],
                 k: [4,2,1,4,2,5],
                 opacity: 0.5,
-                color: '(0,0,0)', 
-                showscale: false            
+                colorscale: [
+                [0, 'rgb(0,0,0)'],
+                [1, 'rgb(0,0,0)']
+            ],
+        opacity: 1.0,
+        showscale: false            
                 }
     return newcube
 
 }
-function Xintegratortotal(xinit, yinit, zinit, number){
+
+function Xintegratortotal(xinit, yinit, zinit, number,a,b,c){
     xx = []
     yy = []
     zz = []
@@ -154,12 +170,12 @@ function Xintegratortotal(xinit, yinit, zinit, number){
         }
         
         if (yinit[j] != 0) {
-                ylim = 1 - xlim
+                ylim = b*(1 - (xlim/a))
             } else {
                 ylim = 0 
             }
         if (zinit[j] != 0) {
-                zlim = 1 - (ylim+xlim)
+                zlim = 1 - c*((ylim/b)+(xlim/a))
             } else {
                 zlim = 0 
             }
@@ -175,8 +191,11 @@ function Xintegratortotal(xinit, yinit, zinit, number){
                 i: [3,0,0,1,3,3],
                 j: [5,1,3,3,0,2],
                 k: [4,2,1,4,2,5],
-                opacity: 0.5,
-                color: 'rgb(255,255,255)', 
+                opacity: 1.0,
+            colorscale: [
+            [0, 'rgb(0,62,116)'],
+            [1, 'rgb(0,62,116)']
+            ],
                 showscale: false            
                 }
     return newcube
@@ -187,10 +206,22 @@ function Xintegratortotal(xinit, yinit, zinit, number){
 var a = 1
 var b = 1
 var c = 1
-
-var zz, yy
+var zinteglimit = (10*c) -1
+var yinteglimit = (10* b) - 1
+var xinteglimit = (10* a)
+var xxinit = [0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.1, 0.1];
+var yyinit = [0., 0.1, 0.1, 0., 0., 0.1, 0.1, 0.];
+var zzinit = [0., 0., 0., 0., 0.1, 0.1, 0.1, 0.1];
+var xxinit2 = [0.0,  0.0,     0.1,      0.1,       0.0, 0.0, 0.1, 0.1];
+var yyinit2 = [0., 0.1,       0.0,       0.1,      0.0, 0.1, 0.1, 0.];
+var zzinit2 = [(0.1), (0.2), (0.1), (0.2), 0.0, 0.0, 0.0, 0.0];
+var xxinit3 = [0.0, 0.0, 0.0, 0.1, 0.1, 0.1];
+var yyinit3 =[0.0, 0.0, b, 0.0, 0.0, (b - 0.1)];
+var zzinit3 =  [0.0, c, 0.0, 0.0, (c - 0.1), 0.0];
+var zz, yy;
 var frames = []
-var data = [{
+
+var tetrahedron = {
     type: "mesh3d",
     x: [0, 0, 0, a],
     y: [0, 0, b, 0],
@@ -200,12 +231,13 @@ var data = [{
     k: [2, 3, 1, 3],
     intensity: [0, 0.33, 0.66, 1],
     colorscale: [
-      [0, 'rgb(0,62,116)'],
-      [1, 'rgb(0,62,116)']
+      [0, 'rgb(255,255,255)'],
+      [1, 'rgb(255,255,255)']
     ],
     opacity: 0.5,
     showscale : false
-  },{
+  }
+var volumeElement = {
     type: "mesh3d",
     x: [0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.1, 0.1] ,
     y: [0., 0.1, 0.1, 0., 0., 0.1, 0.1, 0.],
@@ -222,49 +254,42 @@ var data = [{
         ],
 
     showscale: false
-    }
-];
+}
+var data = [];
 
 
 
-xxinit = [0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.1, 0.1];
-yyinit = [0., 0.1, 0.1, 0., 0., 0.1, 0.1, 0.];
-zzinit = [0., 0., 0., 0., 0.1, 0.1, 0.1, 0.1];
-xxinit2 = [0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.1, 0.1] ,
-yyinit2 = [0., 0.1, 0.1, 0.1, 0., 0.1, 0.1, 0.],
-zzinit2 = [1., 0.9, 0.9, 0.8, 0.0, 0.0, 0.0, 0.0]
-xxinit3 = [0.0, 0.0, 0.0, 0.1, 0.1, 0.1]
-yyinit3 =[0.0, 0.0, 1.0, 0.0, 0.0, 0.9]
-zzinit3 =  [0.0, 1.0, 0.0, 0.0, 0.9, 0.0]
-
-  var frames = [];
-for (var i = 0; i < 8; ++i) {
+for (var i = 0; i < zinteglimit; ++i) {
       frames.push({
-        "data": [Zintegratortotal(xxinit,yyinit,zzinit,i),
-                 Zintegratorelement(xxinit,yyinit,zzinit,i)],
+        "data": [Zintegratortotal(xxinit,yyinit,zzinit,i,a,b,c),
+                 Zintegratorelement(xxinit,yyinit,zzinit,i,a,b,c)],
         "name": i
     })
   }
-for (var i = 0; i < 8; ++i) {
+for (var i = 0; i < yinteglimit; ++i) {
+    var Yintotal = Yintegratortotal(xxinit2,yyinit2,zzinit2,i,a,b,c)
+    var Yinelem = Yintegratorelement(xxinit2,yyinit2,zzinit2,i,a,b,c)
       frames.push({
-        "data": [Yintegratortotal(xxinit2,yyinit2,zzinit2,i),
-                Yintegratorelement(xxinit2,yyinit2,zzinit2,i)],
-        "name": i + 8
+        "data": [Yintotal,Yinelem],
+        "name": i + zinteglimit
     })
   }
-for (var i = 0; i < 10; ++i) {
+for (var i = 0; i < xinteglimit; ++i) {
       frames.push({
-        "data": [Xintegratortotal(xxinit3,yyinit3,zzinit3,i),
-                Xintegratorelement(xxinit3,yyinit3,zzinit3,i)],
-        "name": i + 16
+        "data": [Xintegratortotal(xxinit3,yyinit3,zzinit3,i,a,b,c),
+                Xintegratorelement(xxinit3,yyinit3,zzinit3,i,a,b,c)],
+        "name": i + zinteglimit + yinteglimit
     })
   }
+console.log(frames)
+data.push(frames[0].data[0]);
+data.push(frames[0].data[1]);
+data.push(tetrahedron);
 
 var steps=[]
 
-  for (var i = 0; i < 26; ++i) {
+  for (var i = 0; i < (zinteglimit + yinteglimit + xinteglimit); ++i) {
     steps.push({
-      "label": "R1",
       "method": "animate",
       "args": [
         [i],
@@ -283,7 +308,6 @@ var steps=[]
   }
   var sliders= [{
     "active": 0,
-    "currentvalue": {"prefix": "Trasformation: "},
     "pad": {"t":10},
     "steps": steps
   }];
@@ -315,10 +339,12 @@ var steps=[]
     "showlegend": false,
     "sliders": sliders,
       scene: {
-    xaxis: {range: [-1, 2]},
-    yaxis: {range: [-1, 2]},
-    zaxis: {range: [-1, 2]}
+    xaxis: {range: [-1, a +1]},
+    yaxis: {range: [-1, b + 1]},
+    zaxis: {range: [-1, c + 1]}
     }
+
   }
+
   
 Plotly.newPlot('graph', {data:data, frames: frames, layout: layout})
