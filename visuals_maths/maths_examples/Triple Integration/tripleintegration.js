f8e88ab4c124a389b806398d7a68fb1121e764cb
Unknown <rohan.kamath9@gmail.com>
2017-08-23 12:47:29 +0100

    Intergrates from random point

--- visuals_maths/maths_examples/Triple Integration/tripleintegration.js
+++ visuals_maths/maths_examples/Triple Integration/tripleintegration.js
@@ -1,7 +1,45 @@
-function Zintegratorelement(xinit, yinit,zinit,number, a , b , c ){
+var a = 1, b = 1, c = 1;
+//var zinteglimit = (10*c) -1
+//var yinteglimit = (10* b) - 1
+//var xinteglimit = (10* a)
+var xxinit = [0.3, 0.3, 0.4, 0.4, 0.3, 0.3, 0.4, 0.4];
+var yyinit = [0.2, 0.3, 0.3, 0.2, 0.2, 0.3, 0.3, 0.2];
+var zzinit = [0.4, 0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.5];
+var xxinit2 = [0.4,0.4, 0.3, 0.3, 0.4, 0.4, 0.3, 0.3];
+var yyinit2 = [0.2,0.3, 0.3, 0.2, 0.2, 0.3, 0.3, 0.2];
+var zzinit2 = [0.0,0.0, 0.0, 0.0, 0.4, 0.3, 0.4, 0.5];
+var xxinit3 = [0.3, 0.3, 0.3, 0.4, 0.4, 0.4];
+var yyinit3 = [0.0, 0.0, 0.7, 0.0, 0.0, 0.6];
+var zzinit3 =  [0.0, 0.6, 0.0, 0.0, 0.7, 0.0];
+var zz, yy;
+var frames = [];
+
+function ZintegratorZYXelementup(xinit, yinit,zinit,number){
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
-            zz.push(zinit[j] +(0.1*number))     
+        zz.push(zinit[j] +(0.1*number))     
+    }
+    newcube = {
+        type: "mesh3d",
+        x: xinit ,
+        y: yinit,
+        z: zz,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 1.0,
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
+        intensity: [0, 0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 1],
+        showscale: false
+    }
+    return newcube
+}
+function ZintegratorZYXelementdown(xinit, yinit,zinit,number){
+    zz = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            zz.push(zinit[j] - (0.1*number))     
         }
         newcube = {
         type: "mesh3d",
@@ -20,15 +58,41 @@ function Zintegratorelement(xinit, yinit,zinit,number, a , b , c ){
         showscale: false}
     return newcube
 }
-
-function Zintegratortotal(xinit, yinit,zinit,number,a,b,c){
+function ZintegratorZYXtotalup(xinit, yinit,zinit,number){
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
-            if (zinit[j] != 0){
+            if (zinit[j] != 0.4){
                 zz.push(zinit[j] +(0.1*number))        
             } else 
             {
-                zz.push(0)
+                zz.push(0.0)
+            }
+        }
+        newcube = {
+        type: "mesh3d",
+        x: xinit ,
+        y: yinit,
+        z: zz,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 0.8,
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
+
+        showscale: false, }
+    return newcube
+}
+function ZintegratorZYXtotaldown(xinit, yinit,zinit,number){
+    zz = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            if (zinit[j] != 0.5){
+                zz.push(zinit[j] - (0.1*number))        
+            } else 
+            {
+                zz.push(0.5)
             }
         }
         newcube = {
@@ -49,13 +113,13 @@ function Zintegratortotal(xinit, yinit,zinit,number,a,b,c){
     return newcube
 }
 
-function Yintegratorelement(xinit, yinit, zinit, number,a,b,c){
+function YintegratorZYXelementdown(xinit, yinit, zinit, number){
     yy = []
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
-            ylim = yinit[j] +(0.1*number)
+            ylim = yinit[j] - (0.1*number)
             if (zinit[j] != 0) {
-                zlim = c*(1 - (ylim/b))
+                zlim = c*(0.7 - (ylim/b))
             } else {
                 zlim = 0 
             }
@@ -67,9 +131,9 @@ function Yintegratorelement(xinit, yinit, zinit, number,a,b,c){
         x: xinit,
         y: yy,
         z: zz,
-        i: [7, 2, 5, 0, 3, 5, 2, 4, 0, 0, 4, 4],
-        j: [6, 6, 0, 5, 5, 3, 4, 2, 2, 3, 6, 5],
-        k: [2, 3, 1, 4, 1, 6, 7, 0, 3, 1, 7, 6],
+        i: [0, 3, 0, 4, 4, 7, 3, 7, 1, 5, 4, 4],
+        j: [3, 2, 1, 1, 5, 5, 7, 6, 2, 2, 3, 7],
+        k: [1, 1, 4, 5, 7, 6, 2, 2, 5, 6, 0, 3],
         intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
         opacity: 1.0,
         colorscale: [
@@ -79,18 +143,13 @@ function Yintegratorelement(xinit, yinit, zinit, number,a,b,c){
         showscale: false,}
     return newcube
 }
-
-function Yintegratortotal(xinit, yinit, zinit, number,a,b,c){
+function YintegratorZYXelementup(xinit, yinit, zinit, number){
     yy = []
     zz = []
     for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
-            if (yinit[j] != 0) {
-            ylim = yinit[j] +(0.1*number)
-            } else {
-                ylim = 0 
-            }
+            ylim = yinit[j] + (0.1*number)
             if (zinit[j] != 0) {
-                zlim = c*(1 - (ylim/b))
+                zlim = c*(0.7 - (ylim/b))
             } else {
                 zlim = 0 
             }
@@ -102,9 +161,79 @@ function Yintegratortotal(xinit, yinit, zinit, number,a,b,c){
         x: xinit,
         y: yy,
         z: zz,
-        i: [7, 2, 5, 0, 3, 5, 2, 4, 0, 0, 4, 4],
-        j: [6, 6, 0, 5, 5, 3, 4, 2, 2, 3, 6, 5],
-        k: [2, 3, 1, 4, 1, 6, 7, 0, 3, 1, 7, 6],
+        i: [0, 3, 0, 4, 4, 7, 3, 7, 1, 5, 4, 4],
+        j: [3, 2, 1, 1, 5, 5, 7, 6, 2, 2, 3, 7],
+        k: [1, 1, 4, 5, 7, 6, 2, 2, 5, 6, 0, 3],
+        intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
+        opacity: 1.0,
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
+
+        showscale: false,}
+    return newcube
+}
+function YintegratorZYXtotaldown(xinit, yinit, zinit, number){
+    yy = []
+    zz = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            if (yinit[j] != 0.3) {
+            ylim = yinit[j] - (0.1*number)
+            } else {
+                ylim = 0.2 
+            }
+            if (zinit[j] != 0) {
+                zlim = c*(0.7 - (ylim/b))
+            } else {
+                zlim = 0 
+            }
+            yy.push(ylim)
+            zz.push(zlim)
+        }
+    newcube = {
+        type: "mesh3d",
+        x: xinit,
+        y: yy,
+        z: zz,
+        i: [0, 3, 0, 4, 4, 7, 3, 7, 1, 5, 4, 4],
+        j: [3, 2, 1, 1, 5, 5, 7, 6, 2, 2, 3, 7],
+        k: [1, 1, 4, 5, 7, 6, 2, 2, 5, 6, 0, 3],
+        intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
+        opacity: 0.8,
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
+
+        opacity: 1.0,
+        showscale: false,}
+    return newcube
+}
+function YintegratorZYXtotalup(xinit, yinit, zinit, number){
+    yy = []
+    zz = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            if (yinit[j] != 0.3) {
+            ylim = yinit[j] + (0.1*number)
+            } else {
+                ylim = 0.0 
+            }
+            if (zinit[j] != 0) {
+                zlim = c*(0.7 - (ylim/b))
+            } else {
+                zlim = 0 
+            }
+            yy.push(ylim)
+            zz.push(zlim)
+        }
+    newcube = {
+        type: "mesh3d",
+        x: xinit,
+        y: yy,
+        z: zz,
+        i: [0, 3, 0, 4, 4, 7, 3, 7, 1, 5, 4, 4],
+        j: [3, 2, 1, 1, 5, 5, 7, 6, 2, 2, 3, 7],
+        k: [1, 1, 4, 5, 7, 6, 2, 2, 5, 6, 0, 3],
         intensity: [0, 0.14285714285714285, 0.2857142857142857, 0.42857142857142855, 0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1],
         opacity: 0.8,
         colorscale: [
@@ -117,7 +246,46 @@ function Yintegratortotal(xinit, yinit, zinit, number,a,b,c){
     return newcube
 }
 
-function Xintegratorelement(xinit, yinit, zinit, number,a,b,c){
+function XintegratorZYXelementdown(xinit, yinit, zinit, number){
+    xx = []
+    yy = []
+    zz = []
+    for (var j = 0; j <6 ; j++){
+        xlim = xinit[j] - (0.1*number)
+        if (yinit[j] != 0) {
+                ylim = b*(1 - (xlim/a))
+            } else {
+                ylim = 0 
+            }
+        if (zinit[j] != 0) {
+                zlim = 1 - c*((ylim/b)+(xlim/a))
+            } else {
+                zlim = 0 
+            }
+            xx.push(xlim)
+            yy.push(ylim)
+            zz.push(zlim)
+    }
+            newcube = {
+                type: "mesh3d",
+                x: xx,
+                y: yy,
+                z: zz,
+                i: [3,0,0,1,3,3],
+                j: [5,1,3,3,0,2],
+                k: [4,2,1,4,2,5],
+                opacity: 0.5,
+                colorscale: [
+                [0, 'rgb(0,0,0)'],
+                [1, 'rgb(0,0,0)']
+            ],
+        opacity: 1.0,
+        showscale: false            
+                }
+    return newcube
+
+}
+function XintegratorZYXelementup(xinit, yinit, zinit, number){
     xx = []
     yy = []
     zz = []
@@ -156,17 +324,60 @@ function Xintegratorelement(xinit, yinit, zinit, number,a,b,c){
     return newcube
 
 }
-
-function Xintegratortotal(xinit, yinit, zinit, number,a,b,c){
+function XintegratorZYXtotaldown(xinit, yinit, zinit, number){
     xx = []
     yy = []
     zz = []
     for (var j = 0; j <6 ; j++){
-        if (xinit[j] != 0){
+        if (xinit[j] != 0.3){
+            xlim = xinit[j] - (0.1*number)
+            
+        } else {
+            xlim = 0.3
+        }
+        
+        if (yinit[j] != 0) {
+                ylim = b*(1 - (xlim/a))
+            } else {
+                ylim = 0 
+            }
+        if (zinit[j] != 0) {
+                zlim = 1 - c*((ylim/b)+(xlim/a))
+            } else {
+                zlim = 0 
+            }
+            xx.push(xlim)
+            yy.push(ylim)
+            zz.push(zlim)
+    }
+            newcube = {
+                type: "mesh3d",
+                x: xx,
+                y: yy,
+                z: zz,
+                i: [3,0,0,1,3,3],
+                j: [5,1,3,3,0,2],
+                k: [4,2,1,4,2,5],
+                opacity: 1.0,
+            colorscale: [
+            [0, 'rgb(0,62,116)'],
+            [1, 'rgb(0,62,116)']
+            ],
+                showscale: false            
+                }
+    return newcube
+
+}
+function XintegratorZYXtotalup(xinit, yinit, zinit, number){
+    xx = []
+    yy = []
+    zz = []
+    for (var j = 0; j <6 ; j++){
+        if (xinit[j] != 0.3){
             xlim = xinit[j] + (0.1*number)
             
         } else {
-            xlim = 0
+            xlim = 0.0
         }
         
         if (yinit[j] != 0) {
@@ -202,30 +413,159 @@ function Xintegratortotal(xinit, yinit, zinit, number,a,b,c){
 
 }
 
+function ZYXintegrator() {
+    frames = []
+    for (var i = 0; i < 5; ++i) {
+          frames.push({
+            "data": [ZintegratorZYXtotaldown(xxinit,yyinit,zzinit,i),
+                     ZintegratorZYXelementdown(xxinit,yyinit,zzinit,i)],
+            "name": i})
+      }
+    for (var i = 0; i < 2; ++i) {
+              frames.push({
+            "data": [ZintegratorZYXtotalup(xxinit,yyinit,zzinit,i),
+                     ZintegratorZYXelementup(xxinit,yyinit,zzinit,i)],
+            "name": i + 5})
 
-var a = 1
-var b = 1
-var c = 1
-var zinteglimit = (10*c) -1
-var yinteglimit = (10* b) - 1
-var xinteglimit = (10* a)
-var xxinit = [0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.1, 0.1];
-var yyinit = [0., 0.1, 0.1, 0., 0., 0.1, 0.1, 0.];
-var zzinit = [0., 0., 0., 0., 0.1, 0.1, 0.1, 0.1];
-var xxinit2 = [0.0,  0.0,     0.1,      0.1,       0.0, 0.0, 0.1, 0.1];
-var yyinit2 = [0., 0.1,       0.0,       0.1,      0.0, 0.1, 0.1, 0.];
-var zzinit2 = [(0.1), (0.2), (0.1), (0.2), 0.0, 0.0, 0.0, 0.0];
-var xxinit3 = [0.0, 0.0, 0.0, 0.1, 0.1, 0.1];
-var yyinit3 =[0.0, 0.0, b, 0.0, 0.0, (b - 0.1)];
-var zzinit3 =  [0.0, c, 0.0, 0.0, (c - 0.1), 0.0];
-var zz, yy;
-var frames = []
+    }
+    for (var i = 0; i < 3; ++i) {
+          frames.push({
+            "data": [YintegratorZYXtotaldown(xxinit2,yyinit2,zzinit2,i),
+                YintegratorZYXelementdown(xxinit2,yyinit2,zzinit2,i)],
+            "name": i + 7
+        })
+      }
+    for (var i = 0; i < 3; ++i) {
+        var Yintotal = YintegratorZYXtotalup(xxinit2,yyinit2,zzinit2,i)
+        var Yinelem = YintegratorZYXelementup(xxinit2,yyinit2,zzinit2,i)
+          frames.push({
+            "data": [Yintotal,
+                Yinelem],
+            "name": i + 10
+        })
+      }
+    for (var i = 0; i < 4; ++i) {
+          frames.push({
+            "data": [XintegratorZYXtotaldown(xxinit3,yyinit3,zzinit3,i),
+                    XintegratorZYXelementdown(xxinit3,yyinit3,zzinit3,i)],
+            "name": i + 13
+        })
+      }
+    for (var i = 0; i < 7; ++i) {
+          frames.push({
+            "data": [XintegratorZYXtotalup(xxinit3,yyinit3,zzinit3,i),
+                    XintegratorZYXelementup(xxinit3,yyinit3,zzinit3,i)],
+            "name": i + 17
+        })
+      }
+    return frames
+}
+
+function XintegratorXYZelementup(xinit, yinit,zinit,number){
+    xx = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            xx.push(xinit[j] +(0.1*number))     
+        }
+        newcube = {
+        type: "mesh3d",
+        x: xx ,
+        y: yinit,
+        z: zinit,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 1.0,
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
+        intensity: [0, 0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 1],
+
+        showscale: false}
+    return newcube
+}
+function XintegratorXYZelementdown(xinit, yinit,zinit,number){
+    xx = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            xx.push(xinit[j] - (0.1*number))     
+        }
+        newcube = {
+        type: "mesh3d",
+        x: xx ,
+        y: yinit,
+        z: zinit,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 1.0,
+        colorscale: [
+          [0, 'rgb(0,0,0)'],
+          [1, 'rgb(0,0,0)'] ],
+        intensity: [0, 0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 1],
+
+        showscale: false}
+    return newcube
+}
+function XintegratorXYZtotalup(xinit, yinit,zinit,number){
+    xx = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            if (xinit[j] != 0.3){
+                xx.push(xinit[j] +(0.1*number))        
+            } else 
+            {
+                xx.push(0.0)
+            }
+        }
+        newcube = {
+        type: "mesh3d",
+        x: xx ,
+        y: yinit,
+        z: zinit,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 0.8,
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
+
+        showscale: false, }
+    return newcube
+}
+function XintegratorXYZtotaldown(xinit, yinit,zinit,number){
+    xx = []
+    for (var j = 0 ; j < 8 ; j++) {  //This is to loop through 8 points of the cube
+            if (xinit[j] != 0.4){
+                xx.push(xinit[j] - (0.1*number))        
+            } else 
+            {
+                xx.push(0.4)
+            }
+        }
+        newcube = {
+        type: "mesh3d",
+        x: xx ,
+        y: yinit,
+        z: zinit,
+        i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        opacity: 0.8,
+        colorscale: [
+          [0, 'rgb(0,62,116)'],
+          [1, 'rgb(0,62,116)']
+        ],
+
+        showscale: false, }
+    return newcube
+}
+
 
 var tetrahedron = {
     type: "mesh3d",
-    x: [0, 0, 0, a],
-    y: [0, 0, b, 0],
-    z: [0, c, 0, 0],
+    x: [0, 0, 0, 1],
+    y: [0, 0, 1, 0],
+    z: [0, 1, 0, 0],
     i: [0, 0, 0, 1],
     j: [1, 2, 3, 2],
     k: [2, 3, 1, 3],
@@ -257,55 +597,26 @@ var volumeElement = {
 }
 var data = [];
 
-
-
-for (var i = 0; i < zinteglimit; ++i) {
-      frames.push({
-        "data": [Zintegratortotal(xxinit,yyinit,zzinit,i,a,b,c),
-                 Zintegratorelement(xxinit,yyinit,zzinit,i,a,b,c)],
-        "name": i
-    })
-  }
-for (var i = 0; i < yinteglimit; ++i) {
-    var Yintotal = Yintegratortotal(xxinit2,yyinit2,zzinit2,i,a,b,c)
-    var Yinelem = Yintegratorelement(xxinit2,yyinit2,zzinit2,i,a,b,c)
-      frames.push({
-        "data": [Yintotal,Yinelem],
-        "name": i + zinteglimit
-    })
-  }
-for (var i = 0; i < xinteglimit; ++i) {
-      frames.push({
-        "data": [Xintegratortotal(xxinit3,yyinit3,zzinit3,i,a,b,c),
-                Xintegratorelement(xxinit3,yyinit3,zzinit3,i,a,b,c)],
-        "name": i + zinteglimit + yinteglimit
-    })
-  }
-console.log(frames)
+frames = ZYXintegrator()
 data.push(frames[0].data[0]);
 data.push(frames[0].data[1]);
 data.push(tetrahedron);
 
 var steps=[]
 
-  for (var i = 0; i < (zinteglimit + yinteglimit + xinteglimit); ++i) {
+for (var i = 0; i < (24); ++i) {
     steps.push({
-      "method": "animate",
-      "args": [
-        [i],
-        {
-          "mode": "immediate",
-          "transition": {
-            "duration": 300,
-          },
-          "frame": {
-            "duration": 300,
-            "redraw": false
-          }
-        }
-      ]
+        "method": "animate",
+        "args": [
+                    [i],
+                    {
+                        "mode": "immediate",
+                        "transition": {"duration": 300,},
+                        "frame": {"duration": 300, "redraw": false}
+                    }
+                ]
     })
-  }
+}
   var sliders= [{
     "active": 0,
     "pad": {"t":10},
@@ -325,7 +636,7 @@ var steps=[]
         "buttons": [
           {
             "method": "animate",
-            "args": [null, {"fromcurrent": true, "transition": {"duration": 150, "easing": "quadratic-in-out"}, "frame": {"duration": 150, "redraw":false}}],
+            "args": [null, {"fromcurrent": true, "transition": {"duration": 200, "easing": "quadratic-in-out"}, "frame": {"duration": 200, "redraw":false}}],
             "label": "Play"
           },
           {
@@ -339,12 +650,11 @@ var steps=[]
     "showlegend": false,
     "sliders": sliders,
       scene: {
-    xaxis: {range: [-1, a +1]},
-    yaxis: {range: [-1, b + 1]},
-    zaxis: {range: [-1, c + 1]}
+    xaxis: {range: [-1, 1]},
+    yaxis: {range: [-1, 1]},
+    zaxis: {range: [-1, 1]}
     }
 
   }
 
-  
-Plotly.newPlot('graph', {data:data, frames: frames, layout: layout})
+Plotly.newPlot('graph', {data:data, frames: frames, layout: layout});
