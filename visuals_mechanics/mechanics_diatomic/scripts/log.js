6f59951ceeef42419f6855103a4f209d415124e9
Akash B <akashb50@yahoo.co.uk>
2017-09-08 16:20:32 +0100

    log.js rewrite
    Realised my previous way was stupid, so rewrote the whole thing.

--- visuals_mechanics/mechanics_diatomic/scripts/log.js
+++ visuals_mechanics/mechanics_diatomic/scripts/log.js
@@ -1,50 +1,52 @@
-var checks= [];
-var commands = [];
-var BRANCH = "mechanics";
-var VISUALISATION = "diatomic";
-
-/**
- * Runs sanity check to make sure table and database exists.
- * @param db_name: Name of database to add to.
- */
-function check() {
-    checks.push("CREATE TABLE [IF NOT EXISTS] {db_name}.log (" +
-        "'time' DATETIME PRIMARY KEY NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-        "'visualisation' NOT NULL, " +
-        "'event' TEXT NOT NULL, " +
-        "'target' TEXT NOT NULL, " +
-        "'value' TEXT NOT NULL" +
-        ")", [BRANCH]);
-}
+var BRANCH = "mechanics";                       // Subject branch - determines database name
+var VISUALISATION = "diatomic";                 // Name of visualisation.
+var sessHistory = {};                           // User actions in this session.
+var count = 0;
 
 /**
  * Stores log data in an Array as an SQlite ADD command.
  * @param object: Element clicked on.
  */
 function log(object) {
-    var time, event, target, targetValue;
+    var action = {};
     var currDate = new Date();
+    var targetAttr = [object.tagName, object.id];
 
-    time = currDate.getDate() + "/"
+    action.time = currDate.getDate() + "/"
         + (currDate.getMonth()+1)  + "/"
         + currDate.getFullYear() + " "
         + currDate.getHours() + ":"
         + currDate.getMinutes() + ":"
         + currDate.getSeconds();
 
-    event = object.type;
-    target = object.target.tagName + this.target.class + this.target.id + this.target.display;
-    targetValue = object.target.value;
+    // Event type
+    action.event = object.type;
 
-    commands.push("INSERT INTO {branch}(log) (time, visualisation, event, target, value) " +
-        "VALUES ({time}, {name}, {event}, {target}, {value}", [BRANCH, time, VISUALISATION, event, target, targetValue])
+    if (action.event === "submit") action.targetValue = object.innerHTML;
+    else if (action.event === "") {action.event = "spoiler"; action.targetValue = object.innerHTML.slice(0, 4);}
+    else action.targetValue = object.value;
+
+    // Target element.
+    for (var i = 0; i < targetAttr.length; i++) {
+        if (targetAttr[i] === undefined) console.error('Attribute has undefined value!');
+        else action.target += targetAttr[i] + " ";
+    }
+    action.target = action.target.replace("undefined", "");
+
+    // Name of visualisation.
+    action.visualisation = VISUALISATION;
+
+    // Adding this command to dictionary of sessHistory.
+    count++;
+    sessHistory[count] = action;
+    console.log(action);
 }
 
 /**
- * Before navigating away, send all commands to server.
+ * Before navigating away, send all sessHistory to server.
  */
 window.onbeforeunload = function() {
-    check();
-    $.post("/", {checks: checks, commands: commands});
+    $.post("/database.js", sessHistory);
+    sessHistory = {}; count = 0;
     return confirm("You're about to leave this page, are you sure?");
 };
