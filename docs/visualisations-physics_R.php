166cf46f8b31671df649dc67675179eb4f0f56c7
binaryfunt <binaryfunt@users.noreply.github.com>
2017-09-15 13:31:58 +0100

    Restructure navbar

--- docs/visualisations-physics_R.php
+++ docs/visualisations-physics_R.php
@@ -13,26 +13,57 @@
   <script src="script_R.js"></script>
 </head>
 
-<body id="bso" data-spy="scroll" data-target="#sidebar-nav" data-offset="40">
+<body id="bso" data-spy="scroll" data-target="#sidebar-nav" data-offset="70">
+
+  <?php
+  $usrDepartment = "Physics";
+  $usrYear = 1;
+  ?>
 
   <header>
 
-    <nav id="nav" class="navbar navbar-light bg-light nav-pills nav-justified justify-content-end fixed-top">
+    <nav id="nav" class="navbar navbar-light navbar-expand-sm bg-light nav-pills fixed-top">
       <div class="container-fluid p-0">
+
         <a class="navbar-brand" href="#">Imperial Visualisations</a>
-        <div class="navbar-links">
-          <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDeptDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Department
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDeptDropdown">
-            <a class="dropdown-item" href="#">Physics</a>
-            <a class="dropdown-item" href="#">Maths</a>
-            <a class="dropdown-item" href="#">Chemistry</a>
-          </div>
-          <div class="navbar-divider"></div>
-          <a class="nav-item nav-link" href="#">About</a>
-          <a class="nav-item nav-link" href="#">Help</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDeptDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Department of <?php echo $usrDepartment; ?>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDeptDropdown">
+                <a class="dropdown-item" href="#">Physics</a>
+                <a class="dropdown-item" href="#">Maths</a>
+                <a class="dropdown-item" href="#">Chemistry</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarYearDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Year <?php echo $usrYear; ?>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarYearDropdown">
+                <a class="dropdown-item" href="#">Year 1</a>
+                <a class="dropdown-item" href="#">Year 2</a>
+              </div>
+            </li>
+          </ul>
+
+          <ul class="navbar-nav ml-auto">
+            <!-- <div class="navbar-divider"></div> -->
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Help</a>
+            </li>
+          </ul>
         </div>
+
       </div>
     </nav>
 
@@ -100,7 +131,31 @@
               "ipynb" => "#",
               "py" => "#"
             )
+          ),
+          array(
+            "title" => "Visualisation 4",
+            "imgSrc" => "boundary.png",
+            "openLinks" => array(
+              "web" => "#",
+              "ipynb" => "#"
+            ),
+            "downloadLinks" => array(
+              "ipynb" => "#",
+              "py" => "#"
+            )
           )
+          // array(
+          //   "title" => "Visualisation 5",
+          //   "imgSrc" => "boundary.png",
+          //   "openLinks" => array(
+          //     "web" => "#",
+          //     "ipynb" => "#"
+          //   ),
+          //   "downloadLinks" => array(
+          //     "ipynb" => "#",
+          //     "py" => "#"
+          //   )
+          // )
         );
         ?>
 
