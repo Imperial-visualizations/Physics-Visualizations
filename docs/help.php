e47ea87abe61343bf1e7bf1c63f5346ad4d87d0e
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2017-09-20 16:26:06 +0100

    Create help.php

---
+++ docs/help.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "includes/head.php"; ?>
+
+<body id="bso" data-spy="scroll" data-target="#nav" data-offset="20">
+
+  <?php
+  $loggedIn = 0;
+
+  include "includes/header.php";
+  ?>
+
+
+  <div id="content">
+
+    <div class="jumbotron jumbotron-fluid grad-blue-purple">
+      <div class="container">
+        <h1>Help</h1>
+      </div>
+    </div>
+
+    <div class="jumbotron jumbotron-fluid bg-white">
+      <div class="container">
+        <h4>Browser compatibility</h4>
+        <p>
+          We recommend that you use Chrome to view these visualisations. Firefox, Edge and Opera may work; however, the visualisations have been optimised for Chrome. Internet Explorer is not supported.
+        </p>
+      </div>
+    </div>
+
+  </div>
+
+
+  <?php include "includes/footer.php"; ?>
+
+</body>
+</html>
