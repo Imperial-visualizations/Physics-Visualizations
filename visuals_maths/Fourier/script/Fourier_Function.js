2babe88da319559d4ee46f988ff566637c8b9d3e
Jackson <cw1616@ic.ac.uk>
2018-07-05 15:07:24 +0100

    Fourier Series 1.1

--- visuals_maths/Fourier/script/Fourier_Function.js
+++ visuals_maths/Fourier/script/Fourier_Function.js
@@ -6,11 +6,15 @@ const layout = {
     hovermode: "closest",
     showlegend: false,
     xaxis: {range: [0, 7], zeroline: true, title: "x"},
-    yaxis: {range: [-5, 5], zeroline: true, title: "y"},
+    yaxis: {range: [-6, 6], zeroline: true, title: "y"},
     aspectratio: {x:1, y:1}
 };
 var currentPoint = initialPoint;
 var initX = 0, initY = 0;
+var z = numeric.linspace(0,2*Math.PI,1000);
+// 0 is triangular, 1 is square, 2 is sawtooth
+var shape = 2
+
 
 //Plot
 /**
@@ -19,6 +23,7 @@ var initX = 0, initY = 0;
  */
 function initCarte(type) {
     Plotly.purge("graph");
+    /*
     initX = currentPoint[0];
     initY = currentPoint[1];
 
@@ -27,12 +32,17 @@ function initCarte(type) {
     $("#xControllerDisplay").text(initX);
     $("#yController").val(initY);
     $("#yControllerDisplay").text(initY);
+    */
+
+    var N = parseFloat(document.getElementById('NController').value);
+    var L = parseFloat(document.getElementById('LController').value);
+    var A = parseFloat(document.getElementById('AController').value);
 
 
     if (type === "#maths"){
-        Plotly.newPlot("graph", computeArea(), layout);
-    } else if (type === "#area"){
-        Plotly.newPlot("graph", computeArea(), layout);
+        Plotly.newPlot("graph", computePlot(z), layout);
+    } else if (type === "#plot"){
+        Plotly.newPlot("graph", computePlot(z), layout);
     }
     return;
 }
@@ -42,10 +52,49 @@ function initCarte(type) {
  * @param {float} x - x value.
  * @param {float} y - y value.
  */
-function computeArea() {
-    var x = numeric.linspace(0,2*Math.PI,25);
+
+
+function adding(array){
+    var result = 0
+    for(var i =0; i<array.length; ++i){
+        result+=array[i];
+    }
+    console.log ('the Final result is '+result);
+    return result;
+}
+
+function selection(n,A,L,x,type){
+    if (type===0){
+        formula = (8*A*1/((2*(n)-1) *Math.PI)**2)*(-1)**(n) * Math.sin(x*(2*n -1) *Math.PI /L);
+    } else if (type===1){
+        formula = A/(n*Math.PI) *(1-(-1)**n) *Math.sin(n*Math.PI *x/L);
+    } else if (type===2){
+        formula = 2*A*(-1)**(n+1) /(n*Math.PI) * Math.sin(n *Math.PI* x/L);
+    }
+    return formula
+}
+
+function summation(x) {
+
+    var N = parseFloat(document.getElementById('NController').value);
+    var L = parseFloat(document.getElementById('LController').value);
+    var A = parseFloat(document.getElementById('AController').value);
+
+    n = numeric.linspace(1,N,N);
+
+
     var y = [];
 
+
+    for (var i = 0; i < N; ++i){
+        y.push(selection(n[i],A,L,x,shape));
+        //y.push((8*A/((2*n[i]-1)*Math.PI)**2)*((-1)**n[i])*Math.sin((2*n[i]-1)*Math.PI*x/L));
+    }
+    console.log(y);
+    var sum = adding(y);
+
+    return sum
+/*
     for(var i = 0; i < x.length; ++i){
         y.push(Math.cos(x[i]));
     }
@@ -62,7 +111,37 @@ function computeArea() {
     ];
 
     return data;
+    */
 }
+
+function computePlot(x){
+
+    var N = parseFloat(document.getElementById('NController').value);
+    var L = parseFloat(document.getElementById('LController').value);
+    var A = parseFloat(document.getElementById('AController').value);
+
+    var x_values = []
+    var y_values = []
+
+    for (var i = 0; i < x.length ; ++i){
+        y_values.push(summation(x[i]));
+        x_values.push(x[i]);
+    }
+
+    var data=[
+        {
+            type:"scatter",
+            mode:"lines",
+            x: x_values,
+            y: y_values,
+            line:{color:"rgb(0,225,0)", width:3, dash: "dashed"},
+        },
+    ];
+    return data
+
+
+}
+
 
 /** updates the plot according to the slider controls. */
 function updatePlot() {
@@ -80,7 +159,7 @@ function updatePlot() {
     var L = parseFloat(document.getElementById('LController').value);
     */
 
-    data = computeArea(x, y);
+    data = computePlot(z);
 
     //This is animation bit.
     Plotly.animate(
