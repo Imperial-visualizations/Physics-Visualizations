e650ad3a6334b807f02eb6b2251ce91a67a48cb1
Jackson <cw1616@ic.ac.uk>
2018-07-09 13:02:34 +0100

    Fourier Series 2.1
    Fourier components added

--- visuals_maths/Fourier/script/Fourier_Function.js
+++ visuals_maths/Fourier/script/Fourier_Function.js
@@ -13,7 +13,8 @@ var currentPoint = initialPoint;
 var initX = 0, initY = 0;
 var z = numeric.linspace(0,2*Math.PI,1000);
 // 0 is triangular, 1 is square, 2 is sawtooth, 3 is delta's,
-var shape = 1;
+var shape = 2;
+var decay = 0.9;
 
 
 //Plot
@@ -23,16 +24,6 @@ var shape = 1;
  */
 function initCarte(type) {
     Plotly.purge("graph");
-    /*
-    initX = currentPoint[0];
-    initY = currentPoint[1];
-
-
-    $("#xController").val(initX);
-    $("#xControllerDisplay").text(initX);
-    $("#yController").val(initY);
-    $("#yControllerDisplay").text(initY);
-    */
 
     var N = parseFloat(document.getElementById('NController').value);
     var L = parseFloat(document.getElementById('LController').value);
@@ -43,16 +34,31 @@ function initCarte(type) {
         Plotly.newPlot("graph", computePlot(z), layout);
     } else if (type === "#plot"){
         Plotly.newPlot("graph", computePlot(z), layout);
+    } else if (type ==="#component"){
+        Plotly.newPlot("graph", computeComponents(z), setLayout());
     }
     return;
 }
 
-/**
- * Computes the area element.
- * @param {float} x - x value.
- * @param {float} y - y value.
- */
 
+/* set the layout of the graph to adjust for different amplitudes and different number of terms involved
+it changes the range of the y-axis according to the amplitude and number of therms
+*/
+function setLayout(){
+    var N = parseFloat(document.getElementById('NController').value);
+    var A = parseFloat(document.getElementById('AController').value);
+
+    const new_layout = {
+    width: 450, "height": 500,
+    margin: {l:30, r:30, t:30, b:30},
+    hovermode: "closest",
+    showlegend: false,
+    xaxis: {range: [0, 7], zeroline: true, title: "x"},
+    yaxis: {range: [0, A*N*3], zeroline: true, title: "y"},
+    aspectratio: {x:1, y:1}
+};
+    return new_layout;
+}
 
 function adding(array){
     var result = 0
@@ -73,7 +79,7 @@ function selection(n,A,L,x,type){
     } else if (type===3){
         formula = 1/L * Math.cos(n*Math.PI*x/L);
     }
-    return formula
+    return formula;
 }
 
 function summation(x) {
@@ -92,28 +98,9 @@ function summation(x) {
         y.push(selection(n[i],A,L,x,shape));
         //y.push((8*A/((2*n[i]-1)*Math.PI)**2)*((-1)**n[i])*Math.sin((2*n[i]-1)*Math.PI*x/L));
     }
-    console.log(y);
     var sum = adding(y);
 
-    return sum
-/*
-    for(var i = 0; i < x.length; ++i){
-        y.push(Math.cos(x[i]));
-    }
-
-    var data = [
-        {
-            type: "scatter",
-            mode: "lines",
-            x: x,
-            y: y,
-            line: {color: "rgb(0,200,50)", width: 3, dash: "dashed"},
-        },
-
-    ];
-
-    return data;
-    */
+    return sum;
 }
 
 function computePlot(x){
@@ -122,8 +109,8 @@ function computePlot(x){
     var L = parseFloat(document.getElementById('LController').value);
     var A = parseFloat(document.getElementById('AController').value);
 
-    var x_values = []
-    var y_values = []
+    var x_values = [];
+    var y_values = [];
 
     for (var i = 0; i < x.length ; ++i){
         y_values.push(summation(x[i]));
@@ -139,10 +126,66 @@ function computePlot(x){
             line:{color:"rgb(0,225,0)", width:3, dash: "dashed"},
         },
     ];
-    return data
+    return data;
 
 
 }
+
+function selection2(n,A,type){
+    if (type===0){
+        amplitude = (A*(-1)**n)*(decay)**n; // (8*A*1/((2*(n)-1) *np.pi)**2)*((-1)**(n))
+    } else if (type===1){
+        amplitude = (A*(1-(-1)**n))*(decay)**n; // A/(n*np.pi) *(1-(-1)**n)
+    } else if (type===2){
+        amplitude = (A*(-1)**(n+1))*(decay)**n;  //  2*A*(-1)**(n+1) /(n*np.pi)
+    }
+    return amplitude;
+}
+
+function plotSines(n,x,shape){
+
+    var N = parseFloat(document.getElementById('NController').value);
+    var L = parseFloat(document.getElementById('LController').value);
+    var A = parseFloat(document.getElementById('AController').value);
+
+    var x_n = [];
+    var y_n = [];
+    spacing = 2*A;
+
+    for (var i = 0; i < x.length ; ++i){
+        x_n.push(x[i]);
+        y_n.push((selection2(n,A,shape))*Math.sin(n*Math.PI*x[i]/L)+spacing*n);
+    }
+
+    var data=
+        {
+            type:"scatter",
+            mode:"lines",
+            x: x_n,
+            y: y_n,
+            line:{color:"rgb(0,N*10,0)",width:3, dash:"dashed"},
+        }
+    ;
+    return data;
+
+}
+
+function computeComponents(x){
+
+    var N = parseFloat(document.getElementById('NController').value);
+    var L = parseFloat(document.getElementById('LController').value);
+    var A = parseFloat(document.getElementById('AController').value);
+
+    var data_value=[];
+
+    for (var n=1; n<N+1; ++n){
+        data_value.push(plotSines(n,z,shape));
+    }
+
+    return data_value;
+
+}
+
 
 
 
@@ -161,8 +204,13 @@ function updatePlot() {
     var A = parseFloat(document.getElementById('AController').value);
     var L = parseFloat(document.getElementById('LController').value);
     */
+    if (href==="#plot"){
+        data = computePlot(z);
+    } else if (href==="#component"){
+        initCarte(href);
+        data = computeComponents(z);
+    }
 
-    data = computePlot(z);
 
     //This is animation bit.
     Plotly.animate(
