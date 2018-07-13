025934e8da0c75bb09d80ea3a8c3594aa0f6a18a
Han <XH2416@ic.ac.uk>
2018-07-13 15:30:19 +0100

    made changes

--- visuals_maths/freya/script/green1.js
+++ visuals_maths/freya/script/green1.js
@@ -26,6 +26,7 @@ function plotData() {
     return;
     }
 function initArbitraryShape1() {
+
     var meshSize = 50;
     var phi = numeric.linspace(0,1.99*Math.PI,meshSize);
     var phi2 = numeric.linspace(0,1.98*Math.PI,meshSize);
@@ -47,6 +48,16 @@ function initArbitraryShape1() {
     yTemp1 = yTemp1.concat(yTemp2);
     yTemp1.push(yTemp1[0]);
 
+    arr1 = new Line2d([[xTemp1[2],yTemp1[2]],[xTemp1[6],yTemp1[6]]]);
+    arr2 = new Line2d([[xTemp1[14],yTemp1[14]],[xTemp1[18],yTemp1[18]]]);
+    arr3 = new Line2d([[xTemp1[27],yTemp1[27]],[xTemp1[31],yTemp1[31]]]);
+    arr4 = new Line2d([[xTemp1[39],yTemp1[39]],[xTemp1[43],yTemp1[43]]]);
+    arr5 = new Line2d([[xTemp1[50],yTemp1[50]],[xTemp1[56],yTemp1[56]]]);
+    arr6 = new Line2d([[xTemp1[61],yTemp1[61]],[xTemp1[68],yTemp1[68]]]);
+    arr7 = new Line2d([[xTemp1[74],yTemp1[74]],[xTemp1[80],yTemp1[80]]]);
+    arr8 = new Line2d([[xTemp1[87],yTemp1[87]],[xTemp1[93],yTemp1[93]]]);
+    arr9 = new Line2d([[2,0],[2.8,0]]);
+    arr10 = new Line2d([[4,-0.15],[3.2,-0.15]])
     var curve = [{
         type: "scatter",
         mode: "lines",
@@ -56,8 +67,18 @@ function initArbitraryShape1() {
         fill: 'toself',
         fillcolor: orange,
         opacity: 1
-    }];
-
+        },
+    arr1.arrowHead(black,3),
+    arr2.arrowHead(black,3),
+    arr3.arrowHead(black,3),
+    arr4.arrowHead(black,3),
+    arr5.arrowHead(black,3),
+    arr6.arrowHead(black,3),
+    arr7.arrowHead(black,3),
+    arr8.arrowHead(black,3),
+    arr9.arrowHead(black,3),
+    arr10.arrowHead(black,3)
+    ]
     return curve;
 }
 
@@ -82,7 +103,14 @@ function initArbitraryShape2(){
     xTemp1.push(xTemp1[0]);
     yTemp1 = yTemp1.concat(yTemp2);
     yTemp1.push(yTemp1[0]);
-
+    arr1 = new Line2d([[xTemp1[2],yTemp1[2]],[xTemp1[6],yTemp1[6]]]);
+    arr2 = new Line2d([[xTemp1[14],yTemp1[14]],[xTemp1[18],yTemp1[18]]]);
+    arr3 = new Line2d([[xTemp1[27],yTemp1[27]],[xTemp1[31],yTemp1[31]]]);
+    arr4 = new Line2d([[xTemp1[39],yTemp1[39]],[xTemp1[43],yTemp1[43]]]);
+    arr5 = new Line2d([[xTemp1[50],yTemp1[50]],[xTemp1[56],yTemp1[56]]]);
+    arr6 = new Line2d([[xTemp1[61],yTemp1[61]],[xTemp1[68],yTemp1[68]]]);
+    arr7 = new Line2d([[xTemp1[74],yTemp1[74]],[xTemp1[80],yTemp1[80]]]);
+    arr8 = new Line2d([[xTemp1[87],yTemp1[87]],[xTemp1[93],yTemp1[93]]]);
     var curve = [{
         type: "scatter",
         mode: "lines",
@@ -96,11 +124,20 @@ function initArbitraryShape2(){
         {
         type: "scatter",
         mode: "lines",
-        x: [xTemp1[0],xTemp2[0]],
+        x: [3.975,2.02],
         y: [0,0],
         line: {color: orange, width: 3}
-        }
+        },
+        arr1.arrowHead(black,3),
+        arr2.arrowHead(black,3),
+        arr3.arrowHead(black,3),
+        arr4.arrowHead(black,3),
+        arr5.arrowHead(black,3),
+        arr6.arrowHead(black,3),
+        arr7.arrowHead(black,3),
+        arr8.arrowHead(black,3)
     ];
+
     return curve;
 }
 function main() {
