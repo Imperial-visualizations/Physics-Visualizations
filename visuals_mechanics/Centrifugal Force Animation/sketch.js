7837a192da6f78a0fe75152e0d5384432c2efecb
Unknown <khan_s97@hotmail.co.uk>
2018-07-30 18:51:39 +0100

    Changed visualisation to work with new CSS and finished design of page
    Added text and finished polishing the layout, working visualisation.

--- visuals_mechanics/Centrifugal Force Animation/sketch.js
+++ visuals_mechanics/Centrifugal Force Animation/sketch.js
@@ -4,12 +4,14 @@ let buttonIncrease;
 let buttonDecrease;
 let show = false;
 let img;
-const radius = 250;
-let width = $("#spin").width(), height = 0.8*$(window).height();
+let dragging = true;
+const radius = 160;
+let width = $("#spin").width(), height = 0.6*$(window).height();
+//$("input[type=range]").on('click', () => {dragging != dragging});
+//$("input[type=range]").on('mouseup', () => {dragging = false});
 
 function preload() {
-    //img = loadImage("Earth_flat.jpg");
-    img = loadImage("Earth_smaller.png")
+    img = loadImage("Earth_smaller.png");
 }
 
 
@@ -17,17 +19,12 @@ function preload() {
 function setup() {
     canvas = createCanvas(width,height, WEBGL);
     canvas.parent(spin);
-    //buttonDecrease = createButton('Decrease Speed');
-    //buttonIncrease = createButton('Increase Speed');
-    buttonShow = createButton('Show');
-    /*buttonIncrease.mousePressed(function() {
-        omega += 0.025;
-    })
 
-    buttonDecrease.mousePressed(function() {
-        omega -= 0.025;
-    })
-    */
+    buttonShow = createButton('Show Original');
+    buttonPlaying = createButton('Play/Pause');
+    buttonPlaying.parent(spin);
+    buttonShow.parent(spin);
+
     buttonShow.mouseOver(function() {
         show = !show;
     })
@@ -35,62 +32,38 @@ function setup() {
     buttonShow.mouseOut(function() {
         show = !show;
     })
-    slider = createSlider(0, 1, 0, 0);
-    slider.position(25, 100);
+
+    buttonPlaying.mousePressed(function() {
+        if (playing) {
+        noLoop()
+        playing = false
+    } else {
+        loop()
+        playing = true
+    }
+    });
+
+    slider = createSlider(0, 0.8, 0.025, 0);
+    slider.position(25, 0)
+    slider.parent(animation)
     slider.style('width', '150px');
 }
 
 function draw() {
-  omega = slider.value()
-  let flatteningFactor = 1-omega*omega;
-  //let a = Math.sqrt(radius*radius/(4*flatteningFactor));
-  background(255);
+    omega = slider.value()
+    let flatteningFactor = 1 - omega * omega;
+    let a = Math.sqrt(radius * radius / flatteningFactor);
+    background(15);
+    if (!dragging) {
+        orbitControl();
+    }
   rotateY((2*omega)*millis()/1000);
-  if (show === true) {
+  if (show) {
       noFill();
-      stroke(100);
+      stroke(225);
       sphere(radius);
   }
   fill(173, 25, 96);
   texture(img)
-  ellipsoid(radius*(1+0.1*omega), radius*flatteningFactor, radius*(1+0.1*omega), 125);
+  ellipsoid(a, radius*flatteningFactor, a, 128);
 }
-
-function mousePressed() {
-    if (mouseX < 800 && mouseY < 640 && mouseX > 80 && mouseY > 50) {
-        if (playing) {
-            noLoop()
-            playing = false
-        } else {
-            loop()
-            playing = true
-        }
-    }
-}
-
-function showOriginal() {
-    console.log('This works')
-}
-
-function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        omega -= 0.025
-    }
-
-    if (keyCode === RIGHT_ARROW) {
-        omega += 0.025
-    }
-}
-/*
-function mouseDragged() {
-    if (mouseX > 400){
-        omega += 0.005
-    } else {omega -= 0.005}
-
-    if (omega > 0.9) {
-        omega = 0.9;
-    } else if (omega <= 0) {
-        omega = 0;
-    }
-}
-*/
