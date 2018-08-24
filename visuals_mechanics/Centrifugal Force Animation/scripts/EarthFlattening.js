24269d73fb3a76a3c0b1884ee2e8de9e601039c2
Unknown <khan_s97@hotmail.co.uk>
2018-08-24 15:29:22 +0100

    Changed structure of files and also finished derivation page
    Final stage of visualisation prior to feedback from Clewley. Finished derivation page and added improvements suggested, changed file structure so it is more organised and following convention. Commented code a bit (it's mostly self-explanatory) and added future improvements.

--- visuals_mechanics/Centrifugal Force Animation/scripts/EarthFlattening.js
+++ visuals_mechanics/Centrifugal Force Animation/scripts/EarthFlattening.js
@@ -3,10 +3,10 @@ let omega = 0;
 let buttonIncrease, buttonDecrease, img;
 let show = false;
 const radius = 160;
-let width = $("#spin").width(), height = 0.6 * $(window).height();
+let width = $("#spin").width(), height = 0.6 * $(window).height(); //Set's width to be equal to the div with id spin in the HTML page, ensures it has full width
 
 function preload() {
-    img = loadImage("Earth.png");
+    img = loadImage("../assets/Earth.png");
 }
 
 function setup() {
@@ -36,10 +36,14 @@ function setup() {
     }
     });
 
-    slider = createSlider(0, 0.8, 0.025, 0);
+    slider = createSlider(0, 1, 0.025, 0);
     slider.position(25, 0)
     slider.parent(animation)
     slider.style('width', '150px');
+    sliderLabel = createP("Slider to control ω ")
+    sliderLabel.parent(animation)
+    sliderLabel.position(25,-25)
+    sliderLabel.class("sliderText")
 
     function windowResized() {
         resizeCanvas($("#spin").width(), 0.6*windowHeight);
@@ -49,11 +53,12 @@ function setup() {
 function draw() {
     omega = slider.value()
     let flatteningFactor = 1 - omega * omega;
+    // Mathematical expression for two semi-major axes of the oblate spheroid
     let a = Math.sqrt(radius * radius / flatteningFactor);
-
     background(25);
 
     rotateY((2*omega)*millis()/1000);
+    // Plots the original sphere so user can compare the flattened Earth to a non-rotating Earth in real time
     if (show) {
         noFill()
         stroke(225);
@@ -62,9 +67,10 @@ function draw() {
     fill(173, 25, 96);
     texture(img)
 
+    // Allows user's cursor to direct the light
     let locX = mouseX - height/2;
     let locY = mouseY - width/2;
-    pointLight(255, 255, 255, locX, locY, 500)
+    pointLight(255, 255, 255, 145, -155, 500)
     pointLight(255, 255, 255, locX, locY, 500)
     pointLight(255, 255, 255, locX, locY, 500)
     pointLight(255, 255, 255, locX, locY, 500)
