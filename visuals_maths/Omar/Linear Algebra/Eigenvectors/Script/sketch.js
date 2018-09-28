c6f21cd4ac8432f360eee6981201e874da74294b
Omar Shahpo <omarshahpo@gmail.com>
2018-09-28 22:15:35 +0100

    edit

--- visuals_maths/Omar/Linear Algebra/Eigenvectors/Script/sketch.js
+++ visuals_maths/Omar/Linear Algebra/Eigenvectors/Script/sketch.js
@@ -1,46 +1,103 @@
-//calculates the eigenvectors with their angles for a given matrix
-// input matrix as an array.
-function eigenfunc(matrix) {
-    var xEigVector0 = numeric.eig(matrix).E.x[0][0];
-    var yEigVector0 = numeric.eig(matrix).E.x[1][0];
-    var theta0 = Math.atan2(yEigVector0, xEigVector0);
-
-    var xEigVector1 = numeric.eig(matrix).E.x[0][1];
-    var yEigVector1 = numeric.eig(matrix).E.x[1][1];
-    var theta1 = Math.atan2(yEigVector1, xEigVector1);
-
-    return [
-        [xEigVector0, yEigVector0],
-        [xEigVector1, yEigVector1],
-        theta0,
-        theta1
-    ];
+function setup() {
+  var myCanvas = createCanvas(500, 500);
+  myCanvas.parent("MyContainer");
+  noSmooth();
 }
+var x1, y1, x2, y2;
+x1 = 0.0543;
+y1 = 0.05;
+x2 = 0.03;
+y2 = 0.03;
+matrix = [[x1, y1], [x2, y2]];
 
-//returns array of points in Cartesian space before and after the transformation of a matrix.
-function arrayTransform(matrix, height, width, spacer) {
-    var xArray = [];
-    var yArray = [];
-    var xArrayTrans = [];
-    var yArrayTrans = [];
-                                            //spacer determines the spacing between points.
-    for (var x = -width; x < width + 1; x += spacer) {
-        for (var y = -height; y < height + 1; y += spacer) {
-            xArray.push(x);
-            yArray.push(y);
+eigLines = numeric.eig(matrix).E;
 
-            //transform and generate transArrays.
-            var xTransformed = x1 * x + y1 * y;
-            var yTransformed = x2 * x + y2 * y;
+theta1 = Math.atan2(eigLines.x[0][1], eigLines.x[0][0]);
+theta2 = Math.atan2(eigLines.x[1][1], eigLines.x[1][0]);
 
-            xArrayTrans.push(xTransformed);
-            yArrayTrans.push(yTransformed);
-            return [[xArray,yArray],[xArrayTrans, yArrayTrans]]
-        }
+function draw() {
+  fill(204, 101, 192, 0);
+  //plotting the reference circles
+  strokeWeight(1);
+  stroke(0);
+  ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 200, 200);
+  ellipse(250, 250, 300, 300);
+  ellipse(250, 250, 400, 400);
+
+  //plotting the eigenvector lines
+  strokeWeight(2);
+  stroke(0);
+  line(250, 250, 250 + 1200 * Math.cos(theta1), 250 - 1200 * Math.sin(theta1));
+  line(250, 250, 250 + 1200 * Math.cos(theta2), 250 - 1200 * Math.sin(theta2));
+
+  //plotting axes
+  strokeWeight(0.5);
+  stroke("gray");
+  line(250, 0, 250, 500);
+  line(0, 250, 500, 250);
+
+  //plotting grid
+  strokeWeight(1);
+  for (var i = 0; i < 11; i++) {
+    line((500 * i) / 10, 0, (500 * i) / 10, 500);
+    line(0, (500 * i) / 10, 500, (500 * i) / 10);
+  }
+
+  //def
+  strokeWeight(7);
+  stroke("red");
+
+  //plotting the points of intersection of the iegenvectos with the circles. 
+
+    
+    point(250 + 50 * Math.cos(theta1), 250 - 50 * Math.sin(theta1));
+    
+  point(250 + 50 * Math.cos(theta1), 250 - 50 * Math.sin(theta1));
+
+  point(250 + 100 * Math.cos(theta1), 250 - 100 * Math.sin(theta1));
+
+  point(250 + 150 * Math.cos(theta1), 250 - 150 * Math.sin(theta1));
+
+  point(250 + 200 * Math.cos(theta1), 250 - 200 * Math.sin(theta1));
+
+  stroke("blue");
+
+  point(250 + 50 * Math.cos(theta2), 250 - 50 * Math.sin(theta2));
+
+  point(250 + 50 * Math.cos(theta2), 250 - 50 * Math.sin(theta2));
+
+  point(250 + 100 * Math.cos(theta2), 250 - 100 * Math.sin(theta2));
+
+  point(250 + 150 * Math.cos(theta2), 250 - 150 * Math.sin(theta2));
+
+  point(250 + 200 * Math.cos(theta2), 250 - 200 * Math.sin(theta2));
+
+  //plotting general points:
+  stroke("green");
+
+  for (var i = 0; i < 5; ++i) {
+    for (var j = 1; j < 9; ++j) {
+      if (theta1 + (j * Math.PI) / 4 === theta2) {
+      }
+      point(
+        250 + 50 * i * Math.cos(0 + (j * Math.PI) / 4),
+        250 - 50 * i * Math.sin(0 + (j * Math.PI) / 4)
+      );
+        
     }
+  }
+    
+//transforming points
+    
+   var point1 = math.matrix([250 + 50 * math.cos(theta1),250 - 50 * math.sin(theta1)]) 
+   var point1t = math.multiply(point1, matrix)
+
+   strokeWeight(30)
+   point(point1t._data[0], point1t._data[1])
+    console.log(point1t)
+    
+//plotting transformed points: 
+    
+
 }
-
-
-//generates points on a line at a given angle and returns original and transformed array
-function linePoints(matrix, lineLength, )
-
---
+++ visuals_maths/Omar/Linear Algebra/Eigenvectors/plotlyPeter/selectedLineTransformation_plotlyPeter/Script/sketch.js
@@ -0,0 +1,146 @@
+var layout = {
+    width: 800,
+    height: 800,
+    margin: { l: 20, r: 20, t: 50, b: 50 },
+    hovermode: "closest",
+    showlegend: false,
+    xaxis: { label: "x", range: [-50, 50] },
+    yaxis: { label: "y", range: [-50, 50] },
+    aspectratio: { x: 1, y: 1 }
+};
+
+//define input matrix:
+var x1 = 2;
+var y1 = 4;
+var x2 = 9;
+var y2 = 2;
+
+var matrix = [[x1, y1], [x2, y2]];
+
+//spacer determines distance between points
+var spacer = 10;
+
+var height = 50;
+var width = 50;
+var rho = Math.sqrt(height * height + width * width);
+
+var xArray = [];
+var yArray = [];
+
+var xArrayTrans = [];
+var yArrayTrans = [];
+
+//determine the eigenvectors:
+var xEigVector0 = numeric.eig(matrix).E.x[0][0];
+var yEigVector0 = numeric.eig(matrix).E.x[1][0];
+var theta0 = Math.atan2(yEigVector0, xEigVector0);
+
+var xEigVector1 = numeric.eig(matrix).E.x[0][1];
+var yEigVector1 = numeric.eig(matrix).E.x[1][1];
+var theta1 = Math.atan2(yEigVector1, xEigVector1);
+
+console.log(xEigVector1, yEigVector1);
+
+//defining the input angle from the slider
+
+var inputTheta = 1.5;
+
+//generate eigenvector lines, line of input and transformed points.
+for (var x = -width; x < width + 1; x += spacer) {
+    for (var y = -height; y < height + 1; y += spacer) {
+        xArray.push(x);
+        yArray.push(y);
+
+        //transform and generate transArrays.
+        var xTransformed = x1 * x + y1 * y;
+        var yTransformed = x2 * x + y2 * y;
+
+        xArrayTrans.push(xTransformed);
+        yArrayTrans.push(yTransformed);
+    }
+}
+
+//generate points on the input line.
+var xInput = [];
+var yInput = [];
+var xInputTransformedArray = [];
+var yInputTransformedArray = [];
+
+var inputArray = numeric.linspace(-rho, rho, 15);
+
+for (var i = 0; i < inputArray.length; i++) {
+    var x = inputArray[i] * Math.cos(inputTheta);
+    var y = inputArray[i] * Math.sin(inputTheta);
+    xInput.push(x);
+    yInput.push(y);
+
+    //transform and generate transArrays.
+    var xInputTransformed = x1 * x + y1 * y;
+    var yInputTransformed = x2 * x + y2 * y;
+
+    xInputTransformedArray.push(xInputTransformed);
+    yInputTransformedArray.push(yInputTransformed);
+}
+
+//determine the length of the lines depending on the length of the transformed objects.
+
+var maxX = Math.max.apply(null, xInputTransformedArray); //max transformed X
+var maxY = Math.max.apply(null, yInputTransformedArray); //max transformed Y
+var lineLength = Math.sqrt(maxX * maxX + maxY * maxY);
+
+//defining input line:
+var data = [
+
+    {
+        x: xInput,
+        y: yInput,
+        type: "scatter",
+        mode: "markers"
+    },
+    {
+        x: [-lineLength * math.cos(theta0), lineLength * math.cos(theta0)],
+        y: [-lineLength * math.sin(theta0), lineLength * math.sin(theta0)]
+    },
+    {
+        x: [-lineLength * math.cos(inputTheta) / 1.5, lineLength * math.cos(inputTheta) / 1.5],
+        y: [-lineLength * math.sin(inputTheta) / 1.5, lineLength * math.sin(inputTheta)/ 1.5]
+    },
+
+    {
+        x: [-lineLength * math.cos(theta1), lineLength * math.cos(theta1)],
+        y: [-lineLength * math.sin(theta1), lineLength * math.sin(theta1)]
+    }
+];
+
+// animate
+Plotly.plot("graph", {
+    xaxis: { range: [-30, 30] },
+    yaxis: { range: [-30, 30] }
+}).then(function() {
+    Plotly.addFrames("graph", [
+        {
+            data: [{ x: xInputTransformedArray, y: yInputTransformedArray }],
+            name: "frame1"
+        },
+        {
+            data: [{ x: xInput, y: yInput }],
+            name: "frame2"
+        }
+    ]);
+});
+
+function startAnimation() {
+    Plotly.animate("graph", ["frame1", "frame2"], {
+        frame: [{ duration: 5000 }, { duration: 3000 }],
+        transition: [
+            { duration: 3000, easing: "cubic-in" },
+            { duration: 3000, easing: "cubic-out" }
+        ]
+    });
+}
+
+//main console
+function main() {
+    Plotly.plot("graph", data);
+}
+$(document).ready(main);
